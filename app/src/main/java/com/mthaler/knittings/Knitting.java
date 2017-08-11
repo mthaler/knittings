24585acd0e0f02bab16b0f25e512bb6d5e028ef1
Michael Thaler <michael.thaler@dlr.de>
2017-08-11 13:43:59 +0200

    Added Knitting class.

---
+++ app/src/main/java/com/mthaler/knittings/Knitting.java
@@ -0,0 +1,62 @@
+package com.mthaler.knittings;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Knitting {
+    private final UUID id;
+    private String title;
+    private String description;
+    private Date started;
+    private Date finished;
+
+    public Knitting() {
+        // Generate unique identifier
+        id = UUID.randomUUID();
+        title = "title";
+        description = "description";
+        started = new Date();
+        finished = new Date();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStarted() {
+        return started;
+    }
+
+    public void setStarted(Date started) {
+        this.started = started;
+    }
+
+    public Date getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Date finished) {
+        this.finished = finished;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
