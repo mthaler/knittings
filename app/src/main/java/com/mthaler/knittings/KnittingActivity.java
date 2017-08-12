d1f2c30f1825762d180a1800cab22ee181b7f8f8
Michael Thaler <michael.thaler@dlr.de>
2017-08-12 08:20:08 +0200

    Added comments to KnittingActivity.

--- app/src/main/java/com/mthaler/knittings/KnittingActivity.java
+++ app/src/main/java/com/mthaler/knittings/KnittingActivity.java
@@ -2,11 +2,21 @@ package com.mthaler.knittings;
 
 import android.support.v4.app.Fragment;
 
+/**
+ * KnittingActivity displays a single knitting using KnittingFragment
+ *
+ * The activity is displayed when a new knitting is added or if a knitting is clicked
+ * in the knittings list.
+ *
+ * The id of the knitting that should be displayed must be passed when the activity is started
+ */
 public class KnittingActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        final long knittingId = getIntent().getLongExtra(KnittingFragment.EXTRA_KNITTING_ID, -1);
-        return KnittingFragment.newInstance(knittingId);
+        // get the id of the knitting that should be displayed.  
+        final long id = getIntent().getLongExtra(KnittingFragment.EXTRA_KNITTING_ID, -1);
+        // create a new knitting fragment that will be used by this activity
+        return KnittingFragment.newInstance(id);
     }
 }
