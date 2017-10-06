386b7f9fac01cdb21635d3352cfc68efb7d68741
Michael Thaler <michael.thaler@dlr.de>
2017-10-06 20:03:12 +0200

    Converted MainActivity to Kotlin.

---
+++ app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mthaler.knittings
+
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
+/**
+ * The main activity that gets displayed when the app is started.
+ * It displays a list of knittings. The user can add new knittings or
+ * edit existing ones
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab_create_add_knitting) as FloatingActionButton
+        fab.setOnClickListener {
+            val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
+            knittingListView.addKnitting()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.knittings_list, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+        //            case R.id.menu_item_new_knitting:
+        //                final KnittingListView knittingListView = (KnittingListView) getSupportFragmentManager().findFragmentById(R.id.fragment_knitting_list);
+        //                knittingListView.addKnitting();
+        //                return true;
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
