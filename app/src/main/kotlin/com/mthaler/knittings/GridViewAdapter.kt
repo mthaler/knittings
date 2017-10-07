1dd670d117f48cf32abaae934a617551ea5933cd
Michael Thaler <michael.thaler@dlr.de>
2017-10-07 06:39:52 +0200

    Converted GridViewAdapter to Kotlin.

---
+++ app/src/main/kotlin/com/mthaler/knittings/GridViewAdapter.kt
@@ -0,0 +1,78 @@
+package com.mthaler.knittings
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class GridViewAdapter(context: Context, private val layoutResourceId: Int, private val data: List<Photo>) : ArrayAdapter<Photo>(context, layoutResourceId, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        val h: ViewHolder
+
+        if (row == null) {
+            val inflater = (context as Activity).layoutInflater
+            row = inflater.inflate(layoutResourceId, parent, false)
+            h = ViewHolder()
+            h.imageTitle = row!!.findViewById(R.id.text)
+            h.image = row.findViewById(R.id.image)
+            row.tag = h
+        } else {
+            h = row.tag as ViewHolder
+        }
+
+        if (position < count - 1) {
+            val item = data[position]
+            // we use a view tree observer to get the width and the height of the image view and scale the image accordingly reduce memory usage
+            val viewTreeObserver = h.image!!.viewTreeObserver
+            viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    h.image!!.viewTreeObserver.removeOnPreDrawListener(this)
+                    val width = h.image!!.measuredWidth
+                    val height = h.image!!.measuredHeight
+                    val filename = item.filename.absolutePath
+                    val orientation = PictureUtils.getOrientation(filename)
+                    val photo = PictureUtils.decodeSampledBitmapFromPath(filename, width, height)
+                    val rotatedPhoto = PictureUtils.rotateBitmap(photo, orientation)
+                    h.image!!.setImageBitmap(rotatedPhoto)
+                    h.imageTitle!!.text = item.description
+                    return true
+                }
+            })
+        } else {
+            // we use a view tree observer to get the width and the height of the image view and scale the image accordingly reduce memory usage
+            val viewTreeObserver = h.image!!.viewTreeObserver
+            viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    h.image!!.viewTreeObserver.removeOnPreDrawListener(this)
+                    val width = h.image!!.measuredWidth
+                    val img = BitmapFactory.decodeResource(this@GridViewAdapter.context.resources, R.drawable.add_photo)
+                    val scaled = PictureUtils.resize(img, width, width)
+                    h.image!!.setImageBitmap(scaled)
+                    return true
+                }
+            })
+        }
+
+        return row
+    }
+
+    override fun getCount(): Int {
+        // add an additional element used to display add photo icon
+        return super.getCount() + 1
+    }
+
+    internal class ViewHolder {
+        var imageTitle: TextView? = null
+        var image: ImageView? = null
+    }
+}
+
