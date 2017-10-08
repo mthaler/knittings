a21ef51b8d6ce43d4964205eba6d984f6366b93a
Michael Thaler <michael.thaler@dlr.de>
2017-10-08 07:14:34 +0200

    Removed unused imports from PhotoActivity.

--- app/src/main/kotlin/com/mthaler/knittings/PhotoActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/PhotoActivity.kt
@@ -1,10 +1,8 @@
 package com.mthaler.knittings
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
