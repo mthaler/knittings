79f464bb9cfa6826313a1f5005e2ba95afe981d6
Michael Thaler <michael.thaler@dlr.de>
2017-10-09 18:41:26 +0200

    PhotoActivity now extends AnkoLogger, added some comments.

--- app/src/main/kotlin/com/mthaler/knittings/PhotoActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/PhotoActivity.kt
@@ -6,12 +6,16 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.alert
+import org.jetbrains.anko.debug
 
-class PhotoActivity : AppCompatActivity() {
+/**
+ * Activity that displays a photo and allow to delete the photo and use photo as preview in knitting list
+ */
+class PhotoActivity : AppCompatActivity(), AnkoLogger {
 
     private var parentKnittingID: Long = 0
 
@@ -20,7 +24,7 @@ class PhotoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // get the id of the knitting that should be displayed.
         val id = intent.getLongExtra(EXTRA_PHOTO_ID, -1)
@@ -29,7 +33,7 @@ class PhotoActivity : AppCompatActivity() {
         // save parent knitting id
         parentKnittingID = intent.getLongExtra(KnittingActivity.EXTRA_KNITTING_ID, -1)
 
-        // init knitting
+        // init photo view
         val photoDetailsView = supportFragmentManager.findFragmentById(R.id.fragment_photo) as PhotoDetailsView
         photoDetailsView.init(photo)
     }
@@ -57,11 +61,12 @@ class PhotoActivity : AppCompatActivity() {
                 return true
             }
             R.id.menu_item_set_main_photo -> {
+                // set as default photo
                 val photoDetailsView = supportFragmentManager.findFragmentById(R.id.fragment_photo) as PhotoDetailsView
                 val photo = photoDetailsView.photo
                 val knitting = KnittingsDataSource.getInstance(this).getKnitting(photo!!.knittingID)
                 KnittingsDataSource.getInstance(this).updateKnitting(knitting.copy(defaultPhoto = photo))
-                Log.d(LOG_TAG, "Set $photo as default photo")
+                debug("Set $photo as default photo")
                 val layout = findViewById(R.id.photo_activity_layout) as CoordinatorLayout
                 Snackbar.make(layout, "Used as main photo", Snackbar.LENGTH_SHORT).show()
                 return true
@@ -78,8 +83,6 @@ class PhotoActivity : AppCompatActivity() {
     }
 
     companion object {
-
         val EXTRA_PHOTO_ID = "com.mthaler.knitting.PHOTO_ID"
-        val LOG_TAG = PhotoActivity::class.java.simpleName
     }
 }
