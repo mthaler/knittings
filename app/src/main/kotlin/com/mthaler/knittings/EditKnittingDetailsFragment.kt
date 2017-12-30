0965d16cef7bf84388a9419a11987a5647baeaf1
Michael Thaler <michael.thaler@dlr.de>
2017-12-30 15:44:41 +0100

    More work on EditKnittingDetailsFragment.

--- app/src/main/kotlin/com/mthaler/knittings/EditKnittingDetailsFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/EditKnittingDetailsFragment.kt
@@ -9,6 +9,8 @@ import android.widget.EditText
 import android.widget.TextView
 import com.mthaler.knittings.database.datasource
 import com.mthaler.knittings.model.Knitting
+import java.text.DateFormat
+import java.util.*
 
 class EditKnittingDetailsFragment : Fragment() {
 
@@ -44,6 +46,23 @@ class EditKnittingDetailsFragment : Fragment() {
             }
         })
 
+        textViewStarted = v.findViewById(R.id.knitting_started)
+        textViewStarted.setOnClickListener {
+            val fm = activity!!.supportFragmentManager
+            val dialog = DatePickerFragment.newInstance(knitting!!.started)
+            dialog.setTargetFragment(this, EditKnittingDetailsFragment.REQUEST_STARTED)
+            dialog.show(fm, EditKnittingDetailsFragment.DIALOG_DATE)
+        }
+
+        textViewFinished = v.findViewById(R.id.knitting_finished)
+        textViewFinished.setOnClickListener {
+            val fm = activity!!.supportFragmentManager
+            val dialog = DatePickerFragment.newInstance(if (knitting!!.finished != null) knitting!!.finished else Date())
+            dialog.setTargetFragment(this, REQUEST_FINISHED)
+            dialog.show(fm, DIALOG_DATE)
+        }
+
+
         return v
     }
 
@@ -55,6 +74,18 @@ class EditKnittingDetailsFragment : Fragment() {
 
             val editTextDescription = v.findViewById<EditText>(R.id.knitting_description)
             editTextDescription.setText(knitting.description)
+
+            textViewStarted.text = DateFormat.getDateInstance().format(knitting.started)
+
+            textViewFinished.text = if (knitting!!.finished != null) DateFormat.getDateInstance().format(knitting!!.finished) else ""
         }
+    }
+
+    companion object {
+        private val KNITTING_ID = "knitting_id"
+
+        private val DIALOG_DATE = "date"
+        private val REQUEST_STARTED = 0
+        private val REQUEST_FINISHED = 1
     }
 }
