afcf995c5a53bf24710f8d4e6a68d5f660a889c9
Michael Thaler <michael.thaler@dlr.de>
2018-01-01 10:55:14 +0100

    Added take photo button to photo gallery.

--- app/src/main/kotlin/com/mthaler/knittings/PhotoGalleryActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/PhotoGalleryActivity.kt
@@ -3,6 +3,8 @@ package com.mthaler.knittings
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.mthaler.knittings.database.datasource
 import org.jetbrains.anko.find
 
@@ -27,6 +29,21 @@ class PhotoGalleryActivity : AppCompatActivity() {
             } else {
                 error("Could not get photo gallery fragment")
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.photo_gallery, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_add_photo -> {
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 
