8bc6b0eafb417e25110a883eed133dea64252fed
Michael Thaler <michael.thaler@dlr.de>
2018-01-03 19:01:03 +0100

    Some more work on KnittingsDataSourceTest.

--- app/src/androidTest/kotlin/com/mthaler/knittings/database/KnittingsDataSourceTest.java
+++ app/src/androidTest/kotlin/com/mthaler/knittings/database/KnittingsDataSourceTest.java
@@ -4,17 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.mthaler.knittings.model.Knitting;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -23,12 +19,12 @@ public class KnittingsDataSourceTest {
 
     @Before
     public void initDatabase() {
-        // delete all knittings
-        Context ctx = InstrumentationRegistry.getTargetContext();
-        KnittingsDataSource ds = KnittingsDataSource.Companion.getInstance(ctx);
-        for (Knitting knitting : ds.getAllKnittings()) {
-            ds.deleteKnitting(knitting);
-        }
+        deleteAllKnittings();
+    }
+
+    @After
+    public void cleanDatabase() {
+        deleteAllKnittings();
     }
 
     @Test
@@ -46,5 +42,36 @@ public class KnittingsDataSourceTest {
         knittings.add(knitting2);
         knittings.add(knitting3);
         assertEquals(knittings, ds.getAllKnittings());
+    }
+
+    @Test
+    public void testDeleteKnitting() {
+        // Context of the app under test.
+        Context ctx = InstrumentationRegistry.getTargetContext();
+        KnittingsDataSource ds = KnittingsDataSource.Companion.getInstance(ctx);
+        assertTrue(ds.getAllKnittings().isEmpty());
+        Knitting knitting1 = ds.createKnitting("test knitting 1", "first test knitting", new Date(), null, 3.0, 42.0, 4.0);
+        Knitting knitting2 = ds.createKnitting("test knitting 2", "second test knitting", new Date(), null, 4.0, 43.0, 5.0);
+        Knitting knitting3 = ds.createKnitting("test knitting 3", "third test knitting", new Date(), null, 2.0, 39.0, 3.0);
+        assertEquals(3, ds.getAllKnittings().size());
+        ds.deleteKnitting(knitting2);
+        assertEquals(2, ds.getAllKnittings().size());
+        ArrayList<Knitting> knittings = new ArrayList<>();
+        knittings.add(knitting1);
+        knittings.add(knitting3);
+        assertEquals(knittings, ds.getAllKnittings());
+    }
+
+    @Test
+    public void testDeleteKnittingWithPhoto() {
+
+    }
+
+    private void deleteAllKnittings() {
+        Context ctx = InstrumentationRegistry.getTargetContext();
+        KnittingsDataSource ds = KnittingsDataSource.Companion.getInstance(ctx);
+        for (Knitting knitting : ds.getAllKnittings()) {
+            ds.deleteKnitting(knitting);
+        }
     }
 }
