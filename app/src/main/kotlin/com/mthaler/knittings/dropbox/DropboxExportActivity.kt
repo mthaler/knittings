09bdb390c75952e46e5e8429a47852ae2da4bced
Michael Thaler <michael.thaler@dlr.de>
2018-01-08 07:47:38 +0100

    Added DropboxExportActivity.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportActivity.kt
@@ -0,0 +1,13 @@
+package com.mthaler.knittings.dropbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mthaler.knittings.R
+
+class DropboxExportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dropbox_export)
+    }
+}
