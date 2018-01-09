d3fb1bac539934ef92eaba506aa807a113675469
Michael Thaler <michael.thaler@dlr.de>
2018-01-09 06:38:04 +0100

    Converted DropboxActivity to Kotlin.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxActivity.kt
@@ -0,0 +1,46 @@
+package com.mthaler.knittings.dropbox
+
+import android.support.v7.app.AppCompatActivity
+import com.dropbox.core.android.Auth
+
+/**
+ * Base class for Activities that require auth tokens
+ * Will redirect to auth flow if needed
+ */
+abstract class DropboxActivity : AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+
+        val prefs = getSharedPreferences("dropbox-sample", MODE_PRIVATE)
+        var accessToken = prefs.getString("access-token", null)
+        if (accessToken == null) {
+            accessToken = Auth.getOAuth2Token()
+            if (accessToken != null) {
+                prefs.edit().putString("access-token", accessToken).apply()
+                initAndLoadData(accessToken)
+            }
+        } else {
+            initAndLoadData(accessToken)
+        }
+
+        val uid = Auth.getUid()
+        val storedUid = prefs.getString("user-id", null)
+        if (uid != null && uid != storedUid) {
+            prefs.edit().putString("user-id", uid).apply()
+        }
+    }
+
+    private fun initAndLoadData(accessToken: String) {
+        DropboxClientFactory.init(accessToken)
+        loadData()
+    }
+
+    protected abstract fun loadData()
+
+    protected fun hasToken(): Boolean {
+        val prefs = getSharedPreferences("dropbox-sample", MODE_PRIVATE)
+        val accessToken = prefs.getString("access-token", null)
+        return accessToken != null
+    }
+}
