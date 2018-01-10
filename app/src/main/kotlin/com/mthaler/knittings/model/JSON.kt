aeaedb58ff705d0300394f1c91b10496c6e3dada
Michael Thaler <michael.thaler@dlr.de>
2018-01-10 07:58:51 +0100

    Added method to convert knitting and photo lists to JSON.

--- app/src/main/kotlin/com/mthaler/knittings/model/JSON.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/JSON.kt
@@ -2,7 +2,7 @@ package com.mthaler.knittings.model
 
 import org.json.JSONObject
 import java.text.SimpleDateFormat
-import android.util.Base64
+import org.json.JSONArray
 
 private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
 
@@ -34,5 +34,21 @@ fun Photo.toJSON(): JSONObject {
     result.put("filename", filename.absolutePath)
     result.put("knittingID", knittingID)
     result.put("description", description)
+    return result
+}
+
+fun knittingsToJSON(knittings: List<Knitting>): JSONArray {
+    val result = JSONArray()
+    for(knitting in knittings) {
+        result.put(knitting.toJSON())
+    }
+    return result
+}
+
+fun photosToJSON(photos: List<Photo>): JSONArray {
+    val result = JSONArray()
+    for(photo in photos) {
+        result.put(photo.toJSON())
+    }
     return result
 }
