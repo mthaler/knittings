86ced832747293a1454eb0c19b0479200e9fb277
Michael Thaler <michael.thaler@dlr.de>
2018-01-24 12:52:28 +0100

    Cleaned up AbstractDropboxFragment.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxFragment.kt
@@ -3,28 +3,35 @@ package com.mthaler.knittings.dropbox
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.dropbox.core.android.Auth
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.wtf
 
-abstract class AbstractDropboxFragment : Fragment() {
+abstract class AbstractDropboxFragment : Fragment(), AnkoLogger {
 
     override fun onResume() {
         super.onResume()
 
-        val prefs = context!!.getSharedPreferences(SharedPreferencesName, AppCompatActivity.MODE_PRIVATE)
-        var accessToken = prefs.getString("access-token", null)
-        if (accessToken == null) {
-            accessToken = Auth.getOAuth2Token()
-            if (accessToken != null) {
-                prefs.edit().putString("access-token", accessToken).apply()
+        val ctx = context
+        if (ctx != null) {
+            val prefs = ctx.getSharedPreferences(SharedPreferencesName, AppCompatActivity.MODE_PRIVATE)
+            var accessToken = prefs.getString("access-token", null)
+            if (accessToken == null) {
+                accessToken = Auth.getOAuth2Token()
+                if (accessToken != null) {
+                    prefs.edit().putString("access-token", accessToken).apply()
+                    initAndLoadData(accessToken)
+                }
+            } else {
                 initAndLoadData(accessToken)
             }
-        } else {
-            initAndLoadData(accessToken)
-        }
 
-        val uid = Auth.getUid()
-        val storedUid = prefs.getString("user-id", null)
-        if (uid != null && uid != storedUid) {
-            prefs.edit().putString("user-id", uid).apply()
+            val uid = Auth.getUid()
+            val storedUid = prefs.getString("user-id", null)
+            if (uid != null && uid != storedUid) {
+                prefs.edit().putString("user-id", uid).apply()
+            }
+        } else {
+            wtf("context null")
         }
     }
 
@@ -36,9 +43,15 @@ abstract class AbstractDropboxFragment : Fragment() {
     protected abstract fun loadData()
 
     protected fun hasToken(): Boolean {
-        val prefs = context!!.getSharedPreferences(SharedPreferencesName, AppCompatActivity.MODE_PRIVATE)
-        val accessToken = prefs.getString("access-token", null)
-        return accessToken != null
+        val ctx = context
+        if (ctx != null) {
+            val prefs = context!!.getSharedPreferences(SharedPreferencesName, AppCompatActivity.MODE_PRIVATE)
+            val accessToken = prefs.getString("access-token", null)
+            return accessToken != null
+        } else {
+            wtf("context null")
+            return false
+        }
     }
 
     companion object {
