ed603b802e720b2422de3f81257473a5fd368b81
Michael Thaler <michael.thaler@dlr.de>
2018-07-13 09:21:50 +0200

    Added parseDuration method and test.

--- app/src/main/kotlin/com/mthaler/knittings/utils/TimeUtils.kt
+++ app/src/main/kotlin/com/mthaler/knittings/utils/TimeUtils.kt
@@ -8,4 +8,12 @@ object TimeUtils {
         val hours = millis / (1000 * 60 * 60)
         return String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
+
+    fun parseDuration(s: String): Long {
+        val tokens = s.split(':')
+        val hours = tokens[0].toLong()
+        val minutes = tokens[1].toLong()
+        val seconds = tokens[2].toLong()
+        return 1000L * (hours * 3600 + minutes * 60 + seconds)
+    }
 }
