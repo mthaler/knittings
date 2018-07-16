1448ecade0bf3648615db53a47d96ddf4731b26e
Michael Thaler <michael.thaler@dlr.de>
2018-07-16 16:05:37 +0200

    Refactored ListFolderTask.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/ListFolderTask.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/ListFolderTask.kt
@@ -9,15 +9,11 @@ import com.dropbox.core.v2.files.ListFolderResult
 /**
  * Async task to list items in a folder
  */
-internal class ListFolderTask(private val mDbxClient: DbxClientV2, private val mCallback: Callback) : AsyncTask<String, Void, ListFolderResult>() {
+internal class ListFolderTask(private val dbxClient: DbxClientV2,
+                              private val onDataLoaded: (ListFolderResult) -> Unit,
+                              private val onError: (Exception) -> Unit) : AsyncTask<String, Void, ListFolderResult>() {
 
     private var mException: Exception? = null
-
-    interface Callback {
-        fun onDataLoaded(result: ListFolderResult)
-
-        fun onError(e: Exception)
-    }
 
     override fun onPostExecute(result: ListFolderResult) {
         super.onPostExecute(result)
@@ -25,15 +21,15 @@ internal class ListFolderTask(private val mDbxClient: DbxClientV2, private val m
         val ex = mException
 
         if (ex != null) {
-            mCallback.onError(ex)
+            onError(ex)
         } else {
-            mCallback.onDataLoaded(result)
+            onDataLoaded(result)
         }
     }
 
     override fun doInBackground(vararg params: String): ListFolderResult? {
         try {
-            return mDbxClient.files().listFolder(params[0])
+            return dbxClient.files().listFolder(params[0])
         } catch (e: DbxException) {
             mException = e
         }
