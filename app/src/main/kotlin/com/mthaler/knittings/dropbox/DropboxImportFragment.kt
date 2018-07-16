978c60beeb89a523e7594028e210b57514d492bc
Michael Thaler <michael.thaler@dlr.de>
2018-07-16 13:45:50 +0200

    More work on Dropbox import.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dropbox.core.android.Auth
 import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.fragment_dropbox_import.*
 import org.jetbrains.anko.AnkoLogger
 
 class DropboxImportFragment : AbstractDropboxFragment(), AnkoLogger {
@@ -19,14 +21,52 @@ class DropboxImportFragment : AbstractDropboxFragment(), AnkoLogger {
         retainInstance = true
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dropbox_import, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_dropbox_import, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login_button.setOnClickListener { Auth.startOAuth2Authentication(context, DropboxImportFragment.AppKey) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (hasToken()) {
+            login_button.visibility = View.GONE
+            email_text.visibility = View.VISIBLE
+            name_text.visibility = View.VISIBLE
+            type_text.visibility = View.VISIBLE
+            import_button.isEnabled = true
+            setMode(importing)
+        } else {
+            login_button.visibility = View.VISIBLE
+            email_text.visibility = View.GONE
+            name_text.visibility = View.GONE
+            type_text.visibility = View.GONE
+            import_button.isEnabled = false
+        }
     }
 
     override fun loadData() {
 
+    }
+
+    private fun setMode(importing: Boolean) {
+        if (importing) {
+            import_button.visibility = View.GONE
+            import_text.visibility = View.VISIBLE
+            progressBar.visibility = View.VISIBLE
+            cancel_button.visibility = View.VISIBLE
+        } else {
+            import_button.visibility = View.VISIBLE
+            import_text.visibility = View.GONE
+            progressBar.visibility = View.GONE
+            progressBar.progress = 0
+            cancel_button.visibility = View.GONE
+        }
+        this.importing = importing
     }
 
     companion object {
