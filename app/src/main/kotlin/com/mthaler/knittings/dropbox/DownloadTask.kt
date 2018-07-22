792b7a565afc3a217232d94d14da70392a85fd96
Michael Thaler <michael.thaler@dlr.de>
2018-07-22 16:54:35 +0200

    Added dummy DownloadTask.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadTask.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadTask.kt
@@ -1,5 +1,27 @@
 package com.mthaler.knittings.dropbox
 
-class DownloadTask {
+import android.content.Context
+import android.os.AsyncTask
+import com.dropbox.core.v2.DbxClientV2
+import com.mthaler.knittings.model.Database
 
+class DownloadTask(private val dbxClient: DbxClientV2,
+                   private val context: Context,
+                   private val database: Database) : AsyncTask<Any, Int?, Any?>() {
+
+    override fun doInBackground(params: Array<Any>): Any? {
+        return null
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+
+    }
+
+    override fun onPostExecute(o: Any?) {
+
+    }
+
+    override fun onCancelled() {
+
+    }
 }
