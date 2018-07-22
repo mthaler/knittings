c1d4e7896557694473db7f52313b6a2a145cdab5
Michael Thaler <michael.thaler@dlr.de>
2018-07-22 16:59:38 +0200

    Started to implement DownloadTask.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadTask.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadTask.kt
@@ -4,12 +4,22 @@ import android.content.Context
 import android.os.AsyncTask
 import com.dropbox.core.v2.DbxClientV2
 import com.mthaler.knittings.model.Database
+import com.mthaler.knittings.utils.FileUtils
+import java.io.ByteArrayOutputStream
 
 class DownloadTask(private val dbxClient: DbxClientV2,
                    private val context: Context,
                    private val database: Database) : AsyncTask<Any, Int?, Any?>() {
 
     override fun doInBackground(params: Array<Any>): Any? {
+        for (photo in database.photos) {
+            // Download the file.
+            val out = ByteArrayOutputStream()
+            val filename = "" + photo.id + FileUtils.getExtension(photo.filename.name)
+            dbxClient.files().download(filename).download(out)
+            val bytes = out.toByteArray()
+            println("Downloaded " + bytes.size)
+        }
         return null
     }
 
