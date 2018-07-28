ec9eef6682bfd9d3ded465ce2fe0fa79233b9653
Michael Thaler <michael.thaler@dlr.de>
2018-07-28 15:33:24 +0200

    Added DownloadPhotosService.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadPhotosService.kt
@@ -0,0 +1,11 @@
+package com.mthaler.knittings.dropbox
+
+import android.app.IntentService
+import android.content.Intent
+
+class DownloadPhotosService : IntentService("DownloadPhotosService") {
+
+    override fun onHandleIntent(intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
