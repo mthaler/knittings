146a97334c800ae0856d0fc4f27128cbcd8beaba
Michael Thaler <michael.thaler@dlr.de>
2018-09-02 08:37:15 +0200

    Started to implement DownloadPhotoService.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadPhotosService.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DownloadPhotosService.kt
@@ -2,10 +2,29 @@ package com.mthaler.knittings.dropbox
 
 import android.app.IntentService
 import android.content.Intent
+import com.mthaler.knittings.model.toDatabase
+import com.mthaler.knittings.utils.FileUtils
+import org.json.JSONObject
+import java.io.FileOutputStream
 
 class DownloadPhotosService : IntentService("DownloadPhotosService") {
 
     override fun onHandleIntent(intent: Intent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        if (intent != null) {
+//            val dataString: String = intent.dataString
+//            val json = JSONObject(dataString)
+//            val database = json.toDatabase()
+//            val count = database.photos.size
+//            for ((index, photo) in database.photos.withIndex()) {
+//                // Download the file.
+//                val filename = "/" + directory + "/" + photo.id + "." + FileUtils.getExtension(photo.filename.name)
+//                FileOutputStream(photo.filename).use {
+//                    dbxClient.files().download(filename).download(it)
+//                }
+//            }
+//        }
+        // Gets data from the incoming Intent
+
+
     }
 }
