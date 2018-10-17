ba00faa48e4f3c9ef39e125ea91de51bd30b13f3
Michael Thaler <michael.thaler@dlr.de>
2018-10-17 19:16:19 +0200

    Timer can now be started / stopped.

--- app/src/main/kotlin/com/mthaler/knittings/StopwatchActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/StopwatchActivity.kt
@@ -2,7 +2,10 @@ package com.mthaler.knittings
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class StopwatchActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class StopwatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stopwatch)
+        runTimer()
     }
 
     //Start the stopwatch running when the Start button is clicked.
@@ -28,5 +32,24 @@ class StopwatchActivity : AppCompatActivity() {
     fun onClickReset(view: View) {
         running = false
         seconds = 0
+    }
+
+    private fun runTimer() {
+        val timeView = findViewById<TextView>(R.id.time_view)
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = seconds % 3600 / 60
+                val secs = seconds % 60
+                val time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
+                timeView.setText(time)
+                if (running) {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+
+            }
+        })
     }
 }
