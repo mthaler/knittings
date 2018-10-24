04a8f7a2641846b11c7ff00107032ded0a59f89e
Michael Thaler <michael.thaler@dlr.de>
2018-10-24 20:05:20 +0200

    Added ColorSwatch to display category color in category list.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryAdapter.kt
@@ -1,13 +1,10 @@
 package com.mthaler.knittings.category
 
 import android.content.Context
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import com.mthaler.knittings.R
 import com.mthaler.knittings.model.Category
@@ -28,16 +25,15 @@ class CategoryAdapter(val context: Context, val categories: ArrayList<Category>,
     }
 
     class ViewHolder(val context: Context, itemView: View): RecyclerView.ViewHolder(itemView){
-        val imgColor = itemView.findViewById<ImageView>(R.id.category_list_item_color)
-        val txtName = itemView.findViewById<TextView>(R.id.category_list_item_name)
+        val colorSwatch = itemView.findViewById<ColorSwatch>(R.id.category_list_item_color)
+        val textFieldName = itemView.findViewById<TextView>(R.id.category_list_item_name)
 
         fun bind(category: Category, listener: OnItemClickListener) {
-            val drawable = context.getResources().getDrawable(R.drawable.color_picker_swatch)
-            if (category.color != null) {
-                drawable.setColorFilter(category.color, PorterDuff.Mode.SRC_ATOP)
+            val c = category.color
+            if (c != null) {
+                colorSwatch.color = c
             }
-            imgColor.setImageDrawable(drawable)
-            txtName.text = category.name
+            textFieldName.text = category.name
             itemView.setOnClickListener({v -> listener.onItemClick(category) })
         }
     }
---
+++ app/src/main/kotlin/com/mthaler/knittings/category/ColorSwatch.kt
@@ -0,0 +1,55 @@
+package com.mthaler.knittings.category
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.mthaler.knittings.R
+
+class ColorSwatch(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    constructor(context: Context): this(context, null)
+
+    private var _color = DEFAULT_COLOR
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var size = 0
+
+    var color: Int
+        get() = _color
+        set(value) {
+            _color = value
+            invalidate()
+        }
+
+    init {
+        paint.isAntiAlias = true
+        setupAttributes(attrs)
+    }
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.ColorSwatch, 0, 0)
+        color = typedArray.getColor(R.styleable.ColorSwatch_color, DEFAULT_COLOR)
+        typedArray.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        // call the super method to keep any drawing from the parent side.
+        super.onDraw(canvas)
+        paint.color = _color
+        paint.style = Paint.Style.FILL
+        val radius = size / 2f
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = Math.min(measuredWidth, measuredHeight)
+        setMeasuredDimension(size, size)
+    }
+
+    companion object {
+        private const val DEFAULT_COLOR = Color.RED
+    }
+}
