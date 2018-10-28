e156af7f1aae9234896dcd40a2b479f6920868ec
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2018-10-28 12:31:41 +0100

    Added comments to KnittingListView.

--- app/src/main/kotlin/com/mthaler/knittings/KnittingListView.kt
+++ app/src/main/kotlin/com/mthaler/knittings/KnittingListView.kt
@@ -4,13 +4,32 @@ interface KnittingListView {
 
     fun addKnitting()
 
+    /**
+     * Get the current sorting used for the knitting list
+     */
     fun getSorting(): Sorting
 
+    /**
+     * Set the sorting of the knitting list. updateKnittingList needs to be called after setting the sorting.
+     *
+     * @param sorting sorting
+     */
     fun setSorting(sorting: Sorting)
 
+    /**
+     * Get the current filter used for the filtering the knitting list
+     */
     fun getFilter(): Filter
 
+    /**
+     * Set the filter used for filtering the knitting list. updateKnittingList needs to be called after setting the sorting
+     *
+     * @param filter filter
+     */
     fun setFilter(filter: Filter)
 
+    /**
+     * Updates the knitting list. This needs to be called if the sorting or the filter is changed
+     */
     fun updateKnittingList()
 }
