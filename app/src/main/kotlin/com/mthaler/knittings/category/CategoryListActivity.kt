5500bcdff437beed6b96c97456f23ed6ce639cc5
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2018-10-28 07:10:25 +0100

    Refactored CategoryListActivity.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
@@ -10,12 +10,18 @@ import com.mthaler.knittings.database.datasource
 import com.mthaler.knittings.model.Category
 import org.jetbrains.anko.startActivity
 
+/**
+ * CategoryListActivity displays a list of categories. The user can add a new category by clicking
+ * the floating action button and edit existing categories by clicking the category.
+ */
 class CategoryListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_list)
 
+        // add new category if the user clicks the floating action button and start the
+        // EditCategoryActivity to edit the new category
         val fab = findViewById<FloatingActionButton>(R.id.fab_create_category)
         fab.setOnClickListener({v -> run {
             val category = datasource.createCategory("category", null)
@@ -26,10 +32,23 @@ class CategoryListActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this)
     }
 
+    /**
+     * The onResume method is called when the activity is started or if the user returns from another activity
+     * e.g. the EditCategoryActivity.
+     */
     override fun onResume() {
         super.onResume()
+        updateCategoryList()
+    }
+
+    /**
+     * Updates the list of categories
+     */
+    private fun updateCategoryList() {
         val rv = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = CategoryAdapter(this, datasource.allCategories, object : OnItemClickListener {
+        val categories = datasource.allCategories
+        // start EditCategoryActivity if the users clicks on a category
+        val adapter = CategoryAdapter(this, categories, object : OnItemClickListener {
             override fun onItemClick(item: Category?) {
                 startActivity<EditCategoryActivity>(EditCategoryActivity.EXTRA_CATEGORY_ID to item!!.id)
             }
