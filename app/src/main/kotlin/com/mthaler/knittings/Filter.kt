01d6f668765531699c739f0134236977d5655ec1
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2018-10-28 12:17:13 +0100

    Added comments to knitting list filters.

--- app/src/main/kotlin/com/mthaler/knittings/Filter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/Filter.kt
@@ -3,16 +3,32 @@ package com.mthaler.knittings
 import com.mthaler.knittings.model.Category
 import com.mthaler.knittings.model.Knitting
 
+/**
+ * Filter used for filtering the knitting list displayed to the user
+ */
 interface Filter {
-
+    /**
+     * Filter the knitting list
+     *
+     * @param knittling list
+     * @return filtered knitting list
+     */
     fun filter(knittings: List<Knitting>): List<Knitting>
 }
 
+/**
+ * A filter that does no filtering: it just returns the given knitting list
+ */
 object NoFilter : Filter {
 
     override fun filter(knittings: List<Knitting>): List<Knitting> = knittings
 }
 
+/**
+ * A filter that filters the knitting list by category
+ *
+ * @param category used for filtering
+ */
 data class SingleCategoryFilter(val category: Category) : Filter {
 
     override fun filter(knittings: List<Knitting>): List<Knitting> = knittings.filter { it.category == category }
