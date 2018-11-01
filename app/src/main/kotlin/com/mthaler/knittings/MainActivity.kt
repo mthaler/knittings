0c0f2e7b7e94ca476e3a00ffd246481bd900cdc2
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2018-11-01 11:00:58 +0100

    More work on reimplementing knitting list using RecyclerView.

--- app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
@@ -33,9 +33,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
 
         // set on click handler of floating action button that creates a new knitting
-        fab_create_add_knitting.setOnClickListener {
-            val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
-            knittingListView.addKnitting()
+        fab.setOnClickListener {
+            //val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
+            //knittingListView.addKnitting()
         }
 
         val toggle = ActionBarDrawerToggle(
@@ -68,55 +68,55 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 true
             }
             R.id.menu_item_sort -> {
-                val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
-                val listItems = arrayOf(getString(R.string.sorting_newest_first), getString(R.string.sorting_oldest_first), getString(R.string.sorting_alphabetical))
-                val builder = AlertDialog.Builder(this)
-                val checkedItem = when(knittingListView.getSorting()) {
-                    Sorting.NewestFirst -> 0
-                    Sorting.OldestFirst -> 1
-                    Sorting.Alphabetical -> 2
-                }
-                builder.setSingleChoiceItems(listItems, checkedItem) { dialog, which -> when(which) {
-                    0 -> knittingListView.setSorting(Sorting.NewestFirst)
-                    1 -> knittingListView.setSorting(Sorting.OldestFirst)
-                    2 -> knittingListView.setSorting(Sorting.Alphabetical)
-                  }
-                    knittingListView.updateKnittingList()
-                    dialog.dismiss()
-                }
-                builder.setNegativeButton(R.string.dialog_button_cancel) { dialog, which -> dialog.dismiss() }
-                val dialog = builder.create()
-                dialog.show()
+//                val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
+//                val listItems = arrayOf(getString(R.string.sorting_newest_first), getString(R.string.sorting_oldest_first), getString(R.string.sorting_alphabetical))
+//                val builder = AlertDialog.Builder(this)
+//                val checkedItem = when(knittingListView.getSorting()) {
+//                    Sorting.NewestFirst -> 0
+//                    Sorting.OldestFirst -> 1
+//                    Sorting.Alphabetical -> 2
+//                }
+//                builder.setSingleChoiceItems(listItems, checkedItem) { dialog, which -> when(which) {
+//                    0 -> knittingListView.setSorting(Sorting.NewestFirst)
+//                    1 -> knittingListView.setSorting(Sorting.OldestFirst)
+//                    2 -> knittingListView.setSorting(Sorting.Alphabetical)
+//                  }
+//                    knittingListView.updateKnittingList()
+//                    dialog.dismiss()
+//                }
+//                builder.setNegativeButton(R.string.dialog_button_cancel) { dialog, which -> dialog.dismiss() }
+//                val dialog = builder.create()
+//                dialog.show()
                 true
             }
             R.id.menu_item_filter -> {
-                val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
-                val categories = datasource.allCategories
-                categories.sortedBy { it.name }
-                val listItems = (listOf(getString(R.string.filter_show_all)) + categories.map { it.name }.toList()).toTypedArray()
-                val builder = AlertDialog.Builder(this)
-                val filter = knittingListView.getFilter()
-                val checkedItem = when (filter) {
-                    is NoFilter -> 0
-                    is SingleCategoryFilter -> {
-                        val index = categories.indexOf(filter.category)
-                        index + 1
-                    }
-                    else -> throw Exception("Unknown filter: $filter")
-                }
-                builder.setSingleChoiceItems(listItems, checkedItem) { dialog, which -> when(which) {
-                    0 -> knittingListView.setFilter(NoFilter)
-                    else -> {
-                        val category = categories[which - 1]
-                        knittingListView.setFilter(SingleCategoryFilter(category))
-                    }
-                }
-                    knittingListView.updateKnittingList()
-                    dialog.dismiss()
-                }
-                builder.setNegativeButton(R.string.dialog_button_cancel) { dialog, which -> dialog.dismiss() }
-                val dialog = builder.create()
-                dialog.show()
+//                val knittingListView = supportFragmentManager.findFragmentById(R.id.fragment_knitting_list) as KnittingListView
+//                val categories = datasource.allCategories
+//                categories.sortedBy { it.name }
+//                val listItems = (listOf(getString(R.string.filter_show_all)) + categories.map { it.name }.toList()).toTypedArray()
+//                val builder = AlertDialog.Builder(this)
+//                val filter = knittingListView.getFilter()
+//                val checkedItem = when (filter) {
+//                    is NoFilter -> 0
+//                    is SingleCategoryFilter -> {
+//                        val index = categories.indexOf(filter.category)
+//                        index + 1
+//                    }
+//                    else -> throw Exception("Unknown filter: $filter")
+//                }
+//                builder.setSingleChoiceItems(listItems, checkedItem) { dialog, which -> when(which) {
+//                    0 -> knittingListView.setFilter(NoFilter)
+//                    else -> {
+//                        val category = categories[which - 1]
+//                        knittingListView.setFilter(SingleCategoryFilter(category))
+//                    }
+//                }
+//                    knittingListView.updateKnittingList()
+//                    dialog.dismiss()
+//                }
+//                builder.setNegativeButton(R.string.dialog_button_cancel) { dialog, which -> dialog.dismiss() }
+//                val dialog = builder.create()
+//                dialog.show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
