5614baf184c05684d8f90df86fb4a8318f966d71
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2018-11-02 07:42:59 +0100

    Added OnItemClickListener.

---
+++ app/src/main/kotlin/com/mthaler/knittings/OnItemClickListener.kt
@@ -0,0 +1,8 @@
+package com.mthaler.knittings
+
+import com.mthaler.knittings.model.Knitting
+
+interface OnItemClickListener {
+
+    fun onItemClick(item: Knitting)
+}
--- app/src/main/kotlin/com/mthaler/knittings/category/OnItemClickListener.java
+++ app/src/main/kotlin/com/mthaler/knittings/category/OnItemClickListener.java
@@ -3,5 +3,6 @@ package com.mthaler.knittings.category;
 import com.mthaler.knittings.model.Category;
 
 public interface OnItemClickListener {
+
     void onItemClick(Category item);
 }
