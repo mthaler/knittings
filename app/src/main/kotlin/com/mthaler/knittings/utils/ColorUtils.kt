7ed0021c962da0e40f484ca051349eac09972709
Michael Thaler <michael.thaler@dlr.de>
2018-11-06 12:02:46 +0100

    Updated ColorUtils and ColorUtilsTest.

--- app/src/main/kotlin/com/mthaler/knittings/utils/ColorUtils.kt
+++ app/src/main/kotlin/com/mthaler/knittings/utils/ColorUtils.kt
@@ -2,29 +2,11 @@ package com.mthaler.knittings.utils
 
 object ColorUtils {
 
-    fun colorToHex(color: Int): String {
-        val alpha = android.graphics.Color.alpha(color)
-        val blue = android.graphics.Color.blue(color)
-        val green = android.graphics.Color.green(color)
-        val red = android.graphics.Color.red(color)
-
-        val alphaHex = To00Hex(alpha)
-        val blueHex = To00Hex(blue)
-        val greenHex = To00Hex(green)
-        val redHex = To00Hex(red)
-
-        // hexBinary value: aabbggrr
-        val str = StringBuilder("#")
-        str.append(alphaHex)
-        str.append(blueHex)
-        str.append(greenHex)
-        str.append(redHex)
-
-        return str.toString()
-    }
-
-    private fun To00Hex(value: Int): String {
-        val hex = "00" + Integer.toHexString(value)
-        return hex.substring(hex.length - 2, hex.length)
-    }
+    /**
+     * Formats a color as hex string
+     *
+     * @param color
+     * @return hex string
+     */
+    fun colorToHex(color: Int): String = String.format("#%08X", color)
 }
--- app/src/test/kotlin/com/mthaler/knittings/utils/ColorUtilsTest.kt
+++ app/src/test/kotlin/com/mthaler/knittings/utils/ColorUtilsTest.kt
@@ -8,7 +8,10 @@ class ColorUtilsTest {
 
     @Test
     fun testColorToHex() {
-        val hex0 = ColorUtils.colorToHex(Color.RED)
-        println(hex0)
+        assertEquals("#FFFF0000",ColorUtils.colorToHex(Color.RED))
+        assertEquals("#FF00FF00",ColorUtils.colorToHex(Color.GREEN))
+        assertEquals("#FF0000FF",ColorUtils.colorToHex(Color.BLUE))
+        assertEquals("#FFFFFFFF",ColorUtils.colorToHex(Color.WHITE))
+        assertEquals("#FF000000",ColorUtils.colorToHex(Color.BLACK))
     }
 }
