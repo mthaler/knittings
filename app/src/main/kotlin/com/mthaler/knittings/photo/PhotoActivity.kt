e3dc4eeef71bf622ffeb9d6137bc48bf11d6c705
Michael Thaler <michael.thaler@dlr.de>
2018-11-12 19:30:59 +0100

    It is now possible to navigate up from photo activity.

--- app/src/main/kotlin/com/mthaler/knittings/photo/PhotoActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/photo/PhotoActivity.kt
@@ -1,9 +1,13 @@
 package com.mthaler.knittings.photo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NavUtils
+import android.view.MenuItem
 import com.mthaler.knittings.R
 import com.mthaler.knittings.database.datasource
+import com.mthaler.knittings.details.KnittingDetailsActivity
 import org.jetbrains.anko.AnkoLogger
 import kotlinx.android.synthetic.main.activity_photo.*
 
@@ -11,6 +15,8 @@ import kotlinx.android.synthetic.main.activity_photo.*
  * Activity that displays a photo and allow to delete the photo and use photo as preview in knitting list
  */
 class PhotoActivity : AppCompatActivity(), AnkoLogger {
+
+    private var knittingID = -1L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +35,29 @@ class PhotoActivity : AppCompatActivity(), AnkoLogger {
         val photos = datasource.getAllPhotos(knitting)
         val currentPhoto = photos.indexOfFirst { p -> p.id == id }
 
+        knittingID = knitting.id
+
         pager.offscreenPageLimit = 3
         val pagerAdapter = PhotoPagerAdapter(supportFragmentManager, datasource.getAllPhotos(knitting))
         pager.adapter = pagerAdapter
         if (currentPhoto >= 0) {
             pager.currentItem = currentPhoto
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            // Respond to the action bar's Up/Home button
+            val upIntent: Intent? = NavUtils.getParentActivityIntent(this)
+            if (upIntent == null) {
+                throw IllegalStateException("No Parent Activity Intent")
+            } else {
+                upIntent.putExtra(KnittingDetailsActivity.EXTRA_KNITTING_ID, knittingID)
+                NavUtils.navigateUpTo(this, upIntent)
+            }
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     companion object {
