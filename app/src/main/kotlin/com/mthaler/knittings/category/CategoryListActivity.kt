cab6ffd7eb4bd01ac6b266bc72cce3ed7797a2d5
Michael Thaler <michael.thaler@dlr.de>
2018-11-13 07:52:27 +0100

    Image is now shown if category list is empty.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.mthaler.knittings.R
 import com.mthaler.knittings.database.datasource
 import org.jetbrains.anko.startActivity
@@ -52,6 +53,14 @@ class CategoryListActivity : AppCompatActivity() {
     private fun updateCategoryList() {
         val rv = findViewById<RecyclerView>(R.id.category_recycler_view)
         val categories = datasource.allCategories
+        // show image if category list is empty
+        if (categories.isEmpty()) {
+            category_empty_recycler_view.visibility = View.VISIBLE
+            category_recycler_view.visibility = View.GONE
+        } else {
+            category_empty_recycler_view.visibility = View.GONE
+            category_recycler_view.visibility = View.VISIBLE
+        }
         // start EditCategoryActivity if the users clicks on a category
         val adapter = CategoryAdapter(this, categories, OnItemClickListener { item -> startActivity<EditCategoryActivity>(EditCategoryActivity.EXTRA_CATEGORY_ID to item!!.id) })
         rv.adapter = adapter
