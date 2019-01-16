cb4b48ca8906ae80bf33844b4d4c7bd3fc76bde4
Michael Thaler <michael.thaler@dlr.de>
2019-01-16 19:27:41 +0100

    Up navigation now works for project count activity.

--- app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
@@ -2,11 +2,17 @@ package com.mthaler.knittings
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_project_count.*
 
 class ProjectCountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_count)
+
+        setSupportActionBar(toolbar)
+
+        // enable up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
