1638715fd2308efbd5c5116f013e90b89590dd77
Michael Thaler <michael.thaler@dlr.de>
2019-01-16 19:34:14 +0100

    Added year spinner to project count activity.

--- app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
@@ -3,6 +3,12 @@ package com.mthaler.knittings
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_project_count.*
+import android.widget.ArrayAdapter
+import java.util.*
+import android.widget.Spinner
+
+
+
 
 class ProjectCountActivity : AppCompatActivity() {
 
@@ -14,5 +20,16 @@ class ProjectCountActivity : AppCompatActivity() {
 
         // enable up navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val years = ArrayList<String>()
+        val thisYear = Calendar.getInstance().get(Calendar.YEAR)
+        for (i in 1900..thisYear) {
+            years.add(Integer.toString(i))
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
+
+
+        val spinYear = findViewById(R.id.year_spinner) as Spinner
+        spinYear.setAdapter(adapter)
     }
 }
