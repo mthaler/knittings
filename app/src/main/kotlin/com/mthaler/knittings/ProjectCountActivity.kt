6c9c96a864e0e44406971ad80a5e4cc40ab74345
Michael Thaler <michael.thaler@dlr.de>
2019-01-18 19:43:50 +0100

    Only show years for which knittings exist in project count spinner.

--- app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
@@ -6,9 +6,7 @@ import kotlinx.android.synthetic.main.activity_project_count.*
 import android.widget.ArrayAdapter
 import java.util.*
 import android.widget.Spinner
-
-
-
+import com.mthaler.knittings.database.datasource
 
 class ProjectCountActivity : AppCompatActivity() {
 
@@ -21,14 +19,23 @@ class ProjectCountActivity : AppCompatActivity() {
         // enable up navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val knittings = datasource.allKnittings
+        knittings.sortBy { it.started }
+        val oldest = knittings.firstOrNull()
+
         val years = ArrayList<String>()
         val thisYear = Calendar.getInstance().get(Calendar.YEAR)
-        for (i in 1900..thisYear) {
-            years.add(Integer.toString(i))
+        if (oldest != null) {
+            val c = Calendar.getInstance()
+            c.time = oldest.started
+            for (i in c.get(Calendar.YEAR)..thisYear) {
+                years.add(Integer.toString(i))
+            }
+        } else {
+            years.add(Integer.toString(thisYear))
         }
+        years.reverse()
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
-
-
         val spinYear = findViewById(R.id.year_spinner) as Spinner
         spinYear.setAdapter(adapter)
     }
