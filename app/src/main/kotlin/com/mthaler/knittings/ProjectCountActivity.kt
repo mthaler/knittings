b785de966045c9429320a135eed9387afed9775a
Michael Thaler <michael.thaler@dlr.de>
2019-01-19 08:11:11 +0100

    More work on ProjectCountActivity.

--- app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/ProjectCountActivity.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_project_count.*
 import android.widget.ArrayAdapter
 import java.util.*
 import android.widget.Spinner
+import android.widget.TextView
 import com.mthaler.knittings.database.datasource
 
 class ProjectCountActivity : AppCompatActivity() {
@@ -24,6 +25,8 @@ class ProjectCountActivity : AppCompatActivity() {
         val knittings = datasource.allKnittings
         knittings.sortBy { it.started }
         val oldest = knittings.firstOrNull()
+
+        val textViewProjectCount = findViewById(R.id.projectCount) as TextView
 
         val years = ArrayList<String>()
         val thisYear = Calendar.getInstance().get(Calendar.YEAR)
@@ -42,26 +45,30 @@ class ProjectCountActivity : AppCompatActivity() {
         spinYear.setAdapter(yearAdapter)
         spinYear.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
         }
 
         val categories = datasource.allCategories
         categories.sortedBy { it.name }
-        val categoryAdapter = ArrayAdapter(this, R.layout.my_spinner, listOf(getString(R.string.filter_show_all)) + categories.map { it.name }.toList())
+        val categoryNames = listOf(getString(R.string.filter_show_all)) + categories.map { it.name }.toList()
+        val categoryAdapter = ArrayAdapter(this, R.layout.my_spinner, categoryNames)
         val spinCategory = findViewById(R.id.category_spinner) as Spinner
         spinCategory.setAdapter(categoryAdapter)
         spinCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                val knittings = datasource.allKnittings
+                if (position == 0) {
+                    textViewProjectCount.text = Integer.toString(knittings.size)
+                } else {
+                    val categoryName = categoryNames[position]
+                    textViewProjectCount.text = Integer.toString(knittings.count { it.category != null && it.category.name == categoryName })
+                }
             }
         }
     }
