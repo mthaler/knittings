57d081ddd23fcfb19c5dd4f5d3879c718713a098
Michael Thaler <michael.thaler@dlr.de>
2019-01-22 08:03:56 +0100

    Added StringUtils.containsIgnoreCase.

--- app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
+++ app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
@@ -12,4 +12,25 @@ object StringUtils {
         val pre = "KMGTPE"[exp - 1]
         return String.format("%.1f %sB", bytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
     }
+
+    fun containsIgnoreCase(src: String, what: String): Boolean {
+        val length = what.length
+        if (length == 0)
+            return true // Empty string is contained
+
+        val firstLo = Character.toLowerCase(what[0])
+        val firstUp = Character.toUpperCase(what[0])
+
+        for (i in src.length - length downTo 0) {
+            // Quick check before calling the more expensive regionMatches() method:
+            val ch = src[i]
+            if (ch != firstLo && ch != firstUp)
+                continue
+
+            if (src.regionMatches(i, what, 0, length, ignoreCase = true))
+                return true
+        }
+
+        return false
+    }
 }
