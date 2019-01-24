61bf05954eabb2fc74dd22784e7afb49c48463d4
Michael Thaler <michael.thaler@dlr.de>
2019-01-24 09:06:02 +0100

    Started to implement SelectCategoryActivity.

--- app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
@@ -3,11 +3,17 @@ package com.mthaler.knittings.category
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.activity_select_category.*
 
 class SelectCategoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_category)
+
+        setSupportActionBar(toolbar)
+
+        // enable up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
