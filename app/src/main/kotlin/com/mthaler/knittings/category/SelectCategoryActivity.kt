1d251e5479300f554275e7f1cf79c0c571888b70
Michael Thaler <michael.thaler@dlr.de>
2019-01-26 15:39:46 +0100

    Removed SelectCategoryActivity.onActivityResult.

--- app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
@@ -73,31 +73,6 @@ class SelectCategoryActivity : AppCompatActivity(), CategoryListFragment.OnFragm
         else -> super.onOptionsItemSelected(item)
     }
 
-    /**
-     * Called when an activity you launched exits, giving you the requestCode you started it with, the resultCode it returned,
-     * and any additional data from it. The resultCode will be RESULT_CANCELED if the activity explicitly returned that,
-     * didn't return any result, or crashed during its operation.
-     *
-     * @param requestCode The integer request code originally supplied to startActivityForResult(), allowing you to identify who this result came from.
-     * @param resultCode The integer result code returned by the child activity through its setResult().
-     * @param data An Intent, which can return result data to the caller (various data can be attached to Intent "extras").
-     */
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            return
-        }
-        if (requestCode == REQUEST_EDIT_CATEGORY) {
-            data?.let {
-                val categoryID = it.getLongExtra(Extras.EXTRA_CATEGORY_ID, -1)
-                val i = Intent()
-                i.putExtra(Extras.EXTRA_CATEGORY_ID, categoryID)
-                setResult(Activity.RESULT_OK, i)
-                finish()
-            }
-        }
-    }
-
     companion object {
         private const val REQUEST_EDIT_CATEGORY = 0
     }
