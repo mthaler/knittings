a7a8e49b977032fdf019cc4a3a16b3859d5ca3ef
Michael Thaler <michael.thaler@dlr.de>
2019-01-26 18:53:30 +0100

    Fixed bug in CategoryListActivity.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryListActivity.kt
@@ -1,10 +1,12 @@
 package com.mthaler.knittings.category
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NavUtils
+import android.view.MenuItem
 import com.mthaler.knittings.R
 import com.mthaler.knittings.database.datasource
-import com.mthaler.knittings.Extras.EXTRA_KNITTING_ID
 import kotlinx.android.synthetic.main.activity_category_list.*
 
 /**
@@ -22,17 +24,40 @@ class CategoryListActivity : AppCompatActivity(), CategoryListFragment.OnFragmen
         // enable up navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // get the id of the category that should be displayed.
-        val knittingID = intent.getLongExtra(EXTRA_KNITTING_ID, -1L)
-
         if (savedInstanceState == null) {
-            val f = CategoryListFragment.newInstance(knittingID)
+            val f = CategoryListFragment.newInstance(-1)
             val fm = supportFragmentManager
             val ft = fm.beginTransaction()
             ft.add(R.id.category_list_container, f)
             //ft.addToBackStack(null)
             ft.commit()
         }
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     *
+     * @param item the menu item that was selected.
+     * @return return false to allow normal menu processing to proceed, true to consume it here.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            // Respond to the action bar's Up/Home button
+            val upIntent: Intent? = NavUtils.getParentActivityIntent(this)
+            if (upIntent == null) {
+                throw IllegalStateException("No Parent Activity Intent")
+            } else {
+                val fm = supportFragmentManager
+                val f = fm.findFragmentById(R.id.category_list_container)
+                if (f is EditCategoryFragment) {
+                    fm.popBackStack()
+                } else {
+                    NavUtils.navigateUpTo(this, upIntent)
+                }
+            }
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     override fun createCategory() {
