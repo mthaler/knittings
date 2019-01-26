c6767b8c1bf34468edcde20813c0cfe87293ec71
Michael Thaler <michael.thaler@dlr.de>
2019-01-26 12:12:59 +0100

    More work on SelectCategoryFragment.

--- app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryFragment.kt
@@ -1,12 +1,18 @@
 package com.mthaler.knittings.category
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mthaler.knittings.Extras
 import com.mthaler.knittings.Extras.EXTRA_KNITTING_ID
 import com.mthaler.knittings.R
+import com.mthaler.knittings.database.datasource
 
 class SelectCategoryFragment : Fragment() {
 
@@ -47,7 +53,18 @@ class SelectCategoryFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_select_category, container, false)
 
+        // add new category if the user clicks the floating action button and start the
+        // EditCategoryActivity to edit the new category
+        val fab = v.findViewById<FloatingActionButton>(R.id.fab_create_category)
+        fab.setOnClickListener { view ->
+            val category = datasource.createCategory("", null)
+            val i = Intent(context, EditCategoryActivity::class.java)
+            i.putExtra(Extras.EXTRA_CATEGORY_ID, category.id)
+            startActivityForResult(i, REQUEST_EDIT_CATEGORY)
+        }
 
+        val rv = v.findViewById<RecyclerView>(R.id.category_recycler_view)
+        rv.layoutManager = LinearLayoutManager(context)
 
         return v
     }
@@ -63,10 +80,12 @@ class SelectCategoryFragment : Fragment() {
          */
         @JvmStatic
         fun newInstance(knittingID: Long) =
-                SelectCategoryFragment().apply {
-                    arguments = Bundle().apply {
-                        putLong(EXTRA_KNITTING_ID, knittingID)
-                    }
+            SelectCategoryFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(EXTRA_KNITTING_ID, knittingID)
                 }
+            }
+
+        private const val REQUEST_EDIT_CATEGORY = 0
     }
 }
