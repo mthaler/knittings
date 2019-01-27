c56ae9f9f4a5c030361a91234392d1b99807bc8b
Michael Thaler <michael.thaler@dlr.de>
2019-01-27 12:22:27 +0100

    Added comments to PhotoGalleryFragment.

--- app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryFragment.kt
@@ -23,9 +23,20 @@ import com.mthaler.knittings.Extras.EXTRA_PHOTO_ID
 class PhotoGalleryFragment : Fragment(), AnkoLogger {
 
     private var knitting: Knitting? = null
-
     private lateinit var gridView: GridView
 
+    /**
+     * Called to have the fragment instantiate its user interface view. This is optional, and non-graphical
+     * fragments can return null (which is the default implementation). This will be called between onCreate(Bundle)
+     * and onActivityCreated(Bundle).
+     *
+     * If you return a View from here, you will later be called in onDestroyView() when the view is being released.
+     *
+     * @param inflater the LayoutInflater object that can be used to inflate any views in the fragment
+     * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+     *                  The fragment should not add the view itself, but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
@@ -42,6 +53,20 @@ class PhotoGalleryFragment : Fragment(), AnkoLogger {
         return v
     }
 
+    /**
+     * This method is called if the activity gets destroyed because e.g. the device configuration changes because the device is rotated
+     * We need to store instance variables because they are not automatically restored
+     *
+     * @param savedInstanceState saved instance state
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        val k = knitting
+        if (k != null) {
+            outState.putLong(EXTRA_KNITTING_ID, k.id)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
     override fun onResume() {
         super.onResume()
         val k = knitting
@@ -53,14 +78,6 @@ class PhotoGalleryFragment : Fragment(), AnkoLogger {
             val gridAdapter = GridViewAdapter(a, R.layout.grid_item_layout, photos)
             gridView.adapter = gridAdapter
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        val k = knitting
-        if (k != null) {
-            outState.putLong(EXTRA_KNITTING_ID, k.id)
-        }
-        super.onSaveInstanceState(outState)
     }
 
     fun init(knitting: Knitting) {
