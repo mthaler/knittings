7d45af1979e20019a25ed4333c444cbb553d3f94
Michael Thaler <michael.thaler@dlr.de>
2019-01-27 07:04:20 +0100

    More work on SettingsFragment.

--- app/src/main/kotlin/com/mthaler/knittings/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/settings/SettingsFragment.kt
@@ -1,14 +1,72 @@
 package com.mthaler.knittings.settings
 
 import android.os.Bundle
+import android.preference.ListPreference
+import android.support.v7.preference.Preference
+import android.preference.PreferenceManager
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.mthaler.knittings.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    /**
+     * Called during onCreate(Bundle) to supply the preferences for this fragment. Subclasses are expected to call
+     * setPreferenceScreen(PreferenceScreen) either directly or via helper methods such as addPreferencesFromResource(int).
+     */
     override fun onCreatePreferences(bundle: Bundle, s: String) {
         // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.app_preferences)
+        bindPreferenceSummaryToValue(findPreference("share_photo_size"))
     }
 
+    companion object {
+
+        /**
+         * A preference value change listener that updates the preference's summary
+         * to reflect its new value.
+         */
+        private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
+            val stringValue = value.toString()
+
+            if (preference is ListPreference) {
+                // For list pref_sharing, look up the correct display value in
+                // the preference's 'entries' list.
+                val index = preference.findIndexOfValue(stringValue)
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        if (index >= 0)
+                            preference.entries[index]
+                        else
+                            null)
+
+            } else {
+                // For all other pref_sharing, set the summary to the value's
+                // simple string representation.
+                preference.summary = stringValue
+            }
+            true
+        }
+
+        /**
+         * Binds a preference's summary to its value. More specifically, when the
+         * preference's value is changed, its summary (line of text below the
+         * preference title) is updated to reflect the value. The summary is also
+         * immediately updated upon calling this method. The exact display format is
+         * dependent on the type of preference.
+
+         * @see .sBindPreferenceSummaryToValueListener
+         */
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            // Set the listener to watch for value changes.
+            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
+
+            // Trigger the listener immediately with the preference's
+            // current value.
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.context)
+                            .getString(preference.key, ""))
+        }
+    }
 }
