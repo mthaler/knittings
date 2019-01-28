fe59c44b0ac035dfacdd0c1160a823292844e600
Michael Thaler <michael.thaler@dlr.de>
2019-01-28 06:58:40 +0100

    More work on NeedleTable.

--- app/src/main/kotlin/com/mthaler/knittings/database/table/NeedleTable.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/table/NeedleTable.kt
@@ -13,20 +13,40 @@ object NeedleTable {
     object Cols {
         val ID = "_id"
         val NAME = "name"
+        val DESCRIPTION = "description"
+        val SIZE = "size"
+        val LENGTH = "length"
+        val MATERIAL = "material"
+        val IN_USE = "in_use"
     }
 
     fun create(db: SQLiteDatabase) {
         db.createTable(NEEDLES, true,
                 Cols.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
-                Cols.NAME to TEXT + NOT_NULL)
+                Cols.NAME to TEXT + NOT_NULL,
+                Cols.DESCRIPTION to TEXT + NOT_NULL,
+                Cols.SIZE to TEXT + NOT_NULL,
+                Cols.LENGTH to TEXT + NOT_NULL,
+                Cols.MATERIAL to TEXT + NOT_NULL,
+                Cols.IN_USE to INTEGER)
     }
 
     fun cursorToNeedle(cursor: Cursor): Needle {
         val idIndex = cursor.getColumnIndex(Cols.ID)
         val idName = cursor.getColumnIndex(Cols.NAME)
+        val idDescription = cursor.getColumnIndex(Cols.DESCRIPTION)
+        val idSize = cursor.getColumnIndex(Cols.SIZE)
+        val idLength = cursor.getColumnIndex(Cols.LENGTH)
+        val idMaterial = cursor.getColumnIndex(Cols.MATERIAL)
+        val idInUse = cursor.getColumnIndex(Cols.IN_USE)
 
         val id = cursor.getLong(idIndex)
         val name = cursor.getString(idName)
-        return Needle(id, name)
+        val description = cursor.getString(idDescription)
+        val size = cursor.getString(idSize)
+        val length = cursor.getString(idLength)
+        val material = cursor.getString(idMaterial)
+        val inUse = cursor.getInt(idInUse)
+        return Needle(id, name, description, size, length, material, inUse > 0)
     }
 }
--- app/src/main/kotlin/com/mthaler/knittings/model/Needle.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/Needle.kt
@@ -2,4 +2,4 @@ package com.mthaler.knittings.model
 
 import java.io.Serializable
 
-data class Needle(val id: Long, val name: String) : Serializable
+data class Needle(val id: Long, val name: String, val description: String, val size: String, val length: String, val material: String, val inUse: Boolean) : Serializable
