69230ff76d6c9ff10d4fa9450392606843c17d0e
Michael Thaler <michael.thaler@dlr.de>
2019-01-28 10:34:00 +0100

    Added NeedleListActivity.

---
+++ app/src/main/kotlin/com/mthaler/knittings/needle/NeedleListActivity.kt
@@ -0,0 +1,27 @@
+package com.mthaler.knittings.needle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.activity_needle_list.*
+
+class NeedleListActivity : AppCompatActivity() {
+
+    /**
+     * Called when the activity is starting. This is where most initialization should go: calling setContentView(int)
+     * to inflate the activity's UI, using findViewById(int) to programmatically interact with widgets in the UI,
+     * calling managedQuery(android.net.Uri, String[], String, String[], String) to retrieve cursors for data being displayed, etc.
+     *
+     * @param savedInstanceState Bundle: If the activity is being re-initialized after previously being shut down then this Bundle contains
+     *                           the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_needle_list)
+
+        setSupportActionBar(toolbar)
+
+        // enable up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
