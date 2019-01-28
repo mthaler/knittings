c057f2e9e9ba8a10a43ebe70d819e7afd8cacd92
Michael Thaler <michael.thaler@dlr.de>
2019-01-28 08:47:36 +0100

    Added Extras.EXTRA_NEEDLE_ID.

--- app/src/main/kotlin/com/mthaler/knittings/Extras.kt
+++ app/src/main/kotlin/com/mthaler/knittings/Extras.kt
@@ -3,5 +3,6 @@ package com.mthaler.knittings
 object Extras {
     const val EXTRA_CATEGORY_ID = "com.mthaler.knitting.CATEGORY_ID"
     const val EXTRA_KNITTING_ID = "com.mthaler.knitting.KNITTING_ID"
+    const val EXTRA_NEEDLE_ID = "com.mthaler.knitting.NEEDLE_ID"
     const val EXTRA_PHOTO_ID = "com.mthaler.knitting.PHOTO_ID"
 }
