c126affd755947781b773f7d4ee8a81057268348
Michael Thaler <michael.thaler@dlr.de>
2019-01-30 07:08:50 +0100

    Added code to upgrade database.

--- app/src/main/kotlin/com/mthaler/knittings/database/KnittingDatabaseHelper.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/KnittingDatabaseHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import com.mthaler.knittings.database.table.CategoryTable
 import com.mthaler.knittings.database.table.KnittingTable
+import com.mthaler.knittings.database.table.NeedleTable
 import com.mthaler.knittings.database.table.PhotoTable
 import org.jetbrains.anko.*
 import org.jetbrains.anko.db.*
@@ -22,7 +23,7 @@ class KnittingDatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context
         } catch (_: ClassNotFoundException) {
             "knittings.db"
         }
-        val DB_VERSION = 2
+        val DB_VERSION = 3
 
         private var instance: KnittingDatabaseHelper? = null
 
@@ -64,19 +65,48 @@ class KnittingDatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        if (oldVersion < 2) {
-            info("Updating knitting table from $oldVersion to $newVersion")
-            db.execSQL(KnittingTable.SQL_ADD_DURATION)
-            info("Added duration colomn to knitting table")
-            db.execSQL(KnittingTable.SQL_ADD_CATEGORY)
-            info("Added category ID colomn to knitting table")
+        info("Updating knitting table from $oldVersion to $newVersion")
+        if (oldVersion == 1) {
+            upgrade12(db)
+            upgrade23(db)
+        } else if (oldVersion == 2) {
+            upgrade23(db)
+        }
+    }
 
-            try {
-                CategoryTable.create(db)
-                debug("Category table created")
-            } catch (ex: Exception) {
-                error("Could not create category table", ex)
-            }
+    /**
+     * Upgrades the database from version 1 to 2
+     *
+     * @param db database
+     */
+    private fun upgrade12(db: SQLiteDatabase) {
+        db.execSQL(KnittingTable.SQL_ADD_DURATION)
+        info("Added duration colomn to knitting table")
+        db.execSQL(KnittingTable.SQL_ADD_CATEGORY)
+        info("Added category ID colomn to knitting table")
+
+        try {
+            CategoryTable.create(db)
+            debug("Category table created")
+        } catch (ex: Exception) {
+            error("Could not create category table", ex)
+        }
+    }
+
+    /**
+     * Upgrades the database from version 2 to 3
+     *
+     * @param db database
+     */
+    private fun upgrade23(db: SQLiteDatabase) {
+        db.execSQL(KnittingTable.SQL_ADD_STATUS)
+        info("Added status colomn to knitting table")
+
+        try {
+            NeedleTable.create(db)
+            debug("Needle table created")
+        } catch (ex: Exception) {
+            error("Could not create needle table", ex)
         }
     }
 }
