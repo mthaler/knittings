3784e75395165bd495cfc9e4ac43bd21ea87c9ee
Michael Thaler <michael.thaler@dlr.de>
2019-02-17 12:58:38 +0100

    Added NeedleType enum.

---
+++ app/src/main/kotlin/com/mthaler/knittings/model/NeedleType.kt
@@ -0,0 +1,51 @@
+package com.mthaler.knittings.model
+
+import android.content.Context
+import com.mthaler.knittings.R
+
+enum class NeedleType {
+    OTHER, CIRCULAR, POINT, SET, ROUND, COAT;
+
+    companion object {
+        /**
+         * Formats the status enum as a string using translated string resources
+         *
+         * @param context context
+         * @param status needle type enum value
+         * @return formatted needle type enum value
+         */
+        fun format(context: Context, needleType: NeedleType): String = when(needleType) {
+            OTHER -> context.resources.getString(R.string.needle_type_other)
+            CIRCULAR -> context.resources.getString(R.string.needle_type_circular)
+            POINT -> context.resources.getString(R.string.needle_type_point)
+            SET -> context.resources.getString(R.string.needle_type_set)
+            ROUND -> context.resources.getString(R.string.needle_type_round)
+            COAT -> context.resources.getString(R.string.needle_type_coat)
+        }
+
+        /**
+         * Creates a list of formatted needle type enum values using translated string resources
+         *
+         * @param context context
+         * @return list of formatted needle type enum values
+         */
+        fun formattedValues(context: Context): List<String> = NeedleType.values().map { format(context, it) }
+
+        /**
+         * Parses the needle type enum from a given string
+         *
+         * @param context context
+         * @param statusStr formatted needle type string
+         * @return needle type enum value
+         */
+        fun parse(context: Context, statusStr: String): NeedleType = when(statusStr) {
+            context.resources.getString(R.string.needle_type_other) -> OTHER
+            context.resources.getString(R.string.needle_type_circular) -> CIRCULAR
+            context.resources.getString(R.string.needle_type_point) -> POINT
+            context.resources.getString(R.string.needle_type_set) -> SET
+            context.resources.getString(R.string.needle_type_round) -> ROUND
+            context.resources.getString(R.string.needle_type_coat) -> COAT
+            else -> OTHER
+        }
+    }
+}
