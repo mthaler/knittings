c783851dcb44b0fc928ad2174371eddb10416ce8
Michael Thaler <michael.thaler@dlr.de>
2019-02-17 08:55:01 +0100

    Added comment to Status enum.

--- app/src/main/kotlin/com/mthaler/knittings/model/Status.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/Status.kt
@@ -1,5 +1,8 @@
 package com.mthaler.knittings.model
 
+/**
+ * Enum representing the status of a knitting project
+ */
 enum class Status {
     IN_THE_WORKS, FINISHED, PLANNED, ON_HOLD, DISCARDED
 }
