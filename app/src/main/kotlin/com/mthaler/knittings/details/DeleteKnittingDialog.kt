2f62bd2187974255ec66bf0c89a0c303dedb6588
Michael Thaler <michael.thaler@dlr.de>
2019-02-24 07:31:51 +0100

    Added DeleteKnittingDialog.

---
+++ app/src/main/kotlin/com/mthaler/knittings/details/DeleteKnittingDialog.kt
@@ -0,0 +1,19 @@
+package com.mthaler.knittings.details
+
+import android.app.AlertDialog
+import android.content.Context
+import com.mthaler.knittings.R
+import com.mthaler.knittings.model.Knitting
+
+object DeleteKnittingDialog {
+
+    fun create(context: Context, knitting: Knitting, onDelete: () -> Unit): AlertDialog {
+        // create the dialog
+        val b = AlertDialog.Builder(context)
+        b.setTitle(R.string.delete_knitting_dialog_title)
+        b.setMessage(context.resources.getString(R.string.delete_knitting_dialog_question))
+        b.setPositiveButton(R.string.delete_knitting_dialog_delete_button, { diaglog, which -> onDelete() })
+        b.setNegativeButton(R.string.dialog_button_cancel, {dialog, which -> })
+        return b.create()
+    }
+}
