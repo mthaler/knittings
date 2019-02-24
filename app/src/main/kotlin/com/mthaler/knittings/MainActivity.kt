6efcdb138cffd38950035cd22fb107a27e22e4c6
Michael Thaler <michael.thaler@dlr.de>
2019-02-24 15:31:09 +0100

    Added category_list_action.xml.

--- app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
@@ -341,7 +341,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
              * @return true if the action mode should be created, false if entering this mode should be aborted.
              */
             override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-                val inflater = mode?.getMenuInflater()
+                val inflater = mode?.menuInflater
                 inflater?.inflate(R.menu.knitting_list_action, menu)
                 return true
             }
