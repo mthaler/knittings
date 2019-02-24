c5854a02adbafe536b6f703da32670e3c97ea4eb
Michael Thaler <michael.thaler@dlr.de>
2019-02-24 06:58:38 +0100

    Added knitting_list_action.xml.

--- app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MainActivity.kt
@@ -110,7 +110,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
      */
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.knittings_list, menu)
+        menuInflater.inflate(R.menu.knitting_list, menu)
         configureSearchView(menu)
         return true
     }
