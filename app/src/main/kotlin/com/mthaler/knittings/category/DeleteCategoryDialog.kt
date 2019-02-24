66271b19bca58b88f6f22cd806389e49a065a0f8
Michael Thaler <michael.thaler@dlr.de>
2019-02-24 15:54:13 +0100

    Improved DeleteCategoryDialog.

--- app/src/main/kotlin/com/mthaler/knittings/category/DeleteCategoryDialog.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/DeleteCategoryDialog.kt
@@ -14,7 +14,7 @@ object DeleteCategoryDialog {
         // create the dialog
         val b = AlertDialog.Builder(context)
         b.setTitle(R.string.delete_category_dialog_title)
-        b.setMessage(context.resources.getString(R.string.delete_category_dialog_question))
+        b.setMessage(context.resources.getString(R.string.delete_category_dialog_question, category.name))
         b.setPositiveButton(context.resources.getString(R.string.delete_category_dialog_delete_button), { diaglog, which -> onDelete() })
         b.setNegativeButton(R.string.dialog_button_cancel, { dialog, which -> })
         return b.create()
