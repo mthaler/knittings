2397d7b7ba2d92c88f2405024e11e57fca50f64e
Michael Thaler <michael.thaler@dlr.de>
2020-03-31 18:40:57 +0200

    Changing theme with theme preference works now.

--- app/src/main/kotlin/com/mthaler/knittings/BaseActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/BaseActivity.kt
@@ -4,33 +4,30 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import com.mthaler.knittings.settings.Theme
 
 open class BaseActivity : AppCompatActivity() {
 
     private lateinit var sharedPref: SharedPreferences
-    private var useMintTheme: Boolean = false
+    private lateinit var currentTheme: Theme
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-        useMintTheme = sharedPref.getBoolean(resources.getString(R.string.key_mint_theme), false)
-        setAppTheme()
+        val name = sharedPref.getString("theme", "default")
+        currentTheme = Theme.getTheme(name)
+        setAppTheme(currentTheme)
     }
 
     override fun onResume() {
         super.onResume()
-        val useMint = sharedPref.getBoolean(resources.getString(R.string.key_mint_theme), false)
-        if (useMintTheme != useMint)
+        val name = sharedPref.getString("theme", "default")
+        if (currentTheme.name != name)
             recreate()
     }
 
-    private fun setAppTheme() {
-        val useMint = sharedPref.getBoolean(resources.getString(R.string.key_mint_theme), false)
-        if (useMint) {
-            setTheme(R.style.Theme_App_Mint)
-        } else {
-            setTheme(R.style.AppTheme_NoActionBar)
-        }
+    private fun setAppTheme(theme: Theme) {
+        setTheme(theme.themeId)
     }
 }
