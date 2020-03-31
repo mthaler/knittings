144e6d19a3ee67c59463f39f9d2e8d0c1ecc202b
Michael Thaler <michael.thaler@dlr.de>
2020-03-31 15:53:58 +0200

    Some more work on ColorPreference.

--- app/src/main/kotlin/com/mthaler/knittings/settings/ColorPreference.java
+++ app/src/main/kotlin/com/mthaler/knittings/settings/ColorPreference.java
@@ -1,22 +1,31 @@
 package com.mthaler.knittings.settings;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-
 import androidx.preference.Preference;
+import com.mthaler.knittings.R;
 
 
 public class ColorPreference extends Preference {
 
     public ColorPreference(Context context) {
         super(context);
+        initAttrs(null, 0);
     }
 
     public ColorPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initAttrs(attrs, 0);
     }
 
     public ColorPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        initAttrs(attrs, defStyle);
+    }
+
+    private void initAttrs(AttributeSet attrs, int defStyle) {
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs, R.styleable.ColorPreference, defStyle, defStyle);
     }
 }
