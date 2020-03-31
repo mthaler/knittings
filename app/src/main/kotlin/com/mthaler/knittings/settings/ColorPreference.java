3bbea25f9ac075a7501bbc7b880a3c98591011f0
Michael Thaler <michael.thaler@dlr.de>
2020-03-31 16:25:21 +0200

    More work on ColorPreference.

--- app/src/main/kotlin/com/mthaler/knittings/settings/ColorPreference.java
+++ app/src/main/kotlin/com/mthaler/knittings/settings/ColorPreference.java
@@ -3,11 +3,15 @@ package com.mthaler.knittings.settings;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
 import com.mthaler.knittings.R;
 
-
 public class ColorPreference extends Preference {
+
+    private ColorShape colorShape = ColorShape.CIRCLE;
+    private int value = 0;
 
     public ColorPreference(Context context) {
         super(context);
@@ -27,5 +31,50 @@ public class ColorPreference extends Preference {
     private void initAttrs(AttributeSet attrs, int defStyle) {
         TypedArray a = getContext().getTheme().obtainStyledAttributes(
                 attrs, R.styleable.ColorPreference, defStyle, defStyle);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        ImageView colorView = (ImageView) holder.findViewById(R.id.color_view);
+        if (colorView != null) {
+            ColorUtils.setColorViewValue(colorView, value, false, colorShape);
+        }
+    }
+
+    public void setValue(int value) {
+        if (callChangeListener(value)) {
+            this.value = value;
+            persistInt(value);
+            notifyChanged();
+        }
+    }
+
+    @Override
+    protected void onClick() {
+        super.onClick();
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, 0);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        setValue(restoreValue ? getPersistedInt(0) : (Integer) defaultValue);
+    }
+
+    public String getFragmentTag() {
+        return "color_" + getKey();
+    }
+
+    public int getValue() {
+        return value;
     }
 }
