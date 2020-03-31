db96f2229cc11e85b95bdddf18afb9832bf00e21
Michael Thaler <michael.thaler@dlr.de>
2020-03-31 17:49:17 +0200

    Added some methods to Theme class.

--- app/src/main/kotlin/com/mthaler/knittings/settings/Theme.kt
+++ app/src/main/kotlin/com/mthaler/knittings/settings/Theme.kt
@@ -1,3 +1,18 @@
 package com.mthaler.knittings.settings
 
-data class Theme(val name: String, val color: Int)
+import com.mthaler.knittings.R
+
+data class Theme(val name: String, val color: Int) {
+
+    companion object {
+
+        val default = Theme("default", R.color.colorPrimary)
+
+        val themes = arrayListOf(default, Theme("mint", R.color.mintColorPrimary))
+
+        fun getTheme(name: String): Theme {
+            val result = themes.find { it.name == name }
+            return if (result != null) result else default
+        }
+    }
+}
