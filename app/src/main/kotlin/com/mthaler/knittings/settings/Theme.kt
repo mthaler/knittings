38ad3dd0cb343487a1c02841d3c0435d032a9227
Michael Thaler <michael.thaler@dlr.de>
2020-04-01 13:31:47 +0200

    Added sky theme.

--- app/src/main/kotlin/com/mthaler/knittings/settings/Theme.kt
+++ app/src/main/kotlin/com/mthaler/knittings/settings/Theme.kt
@@ -11,8 +11,9 @@ data class Theme(val name: String, val colorId: Int, val themeId: Int) {
         val default = Theme("default", R.color.colorPrimary, R.style.AppTheme_NoActionBar)
         val mint = Theme("mint", R.color.mintColorPrimary, R.style.Theme_App_Mint)
         val rose = Theme("rose", R.color.roseColorPrimary, R.style.Theme_App_Rose)
+        val sky = Theme("sky", R.color.skyColorPrimary, R.style.Theme_App_Sky)
 
-        val themes = listOf(default, mint, rose)
+        val themes = listOf(default, mint, rose, sky)
 
         fun getTheme(name: String): Theme {
             val result = themes.find { it.name == name }
