ebc0cfb6ae7ee2aa6a47d11a3506c8e45d0bf189
Michael Thaler <michael.thaler@dlr.de>
2020-04-05 17:21:53 +0200

    Implemented EditCategoryViewModel.

--- app/src/main/kotlin/com/mthaler/knittings/category/EditCategoryViewModel.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/EditCategoryViewModel.kt
@@ -1,5 +1,24 @@
 package com.mthaler.knittings.category
 
-class EditCategoryViewModel {
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.mthaler.knittings.DatasourceViewModel
+import com.mthaler.knittings.model.Category
+import com.mthaler.knittings.utils.setMutVal
 
+class EditCategoryViewModel(application: Application) : DatasourceViewModel(application) {
+
+    private var categoryID = -1L
+    val category = MutableLiveData<Category>()
+
+    fun initCategory(id: Long) {
+        categoryID = id
+        val c = datasource.getCategory(id)
+        category.setMutVal(c)
+    }
+
+    override fun databaseChanged() {
+        val c = datasource.getCategory(categoryID)
+        category.setMutVal(c)
+    }
 }
