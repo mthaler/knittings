1a146a3e793f85c211a164ecbdc963b927cdc52c
Michael Thaler <michael.thaler@dlr.de>
2020-04-05 16:18:09 +0200

    Added CategoryListViewModel.

---
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryListViewModel.kt
@@ -0,0 +1,22 @@
+package com.mthaler.knittings.category
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.mthaler.knittings.database.DatabaseObserver
+import com.mthaler.knittings.database.KnittingsDataSource
+import com.mthaler.knittings.utils.setMutVal
+
+class CategoryListViewModel(application: Application): AndroidViewModel(application), DatabaseObserver {
+
+    val datasource = KnittingsDataSource.getInstance(application.applicationContext)
+    private val categories = MutableLiveData(datasource.allCategories)
+
+    init {
+        datasource.addObserver(this)
+    }
+
+    override fun databaseChanged() {
+        categories.setMutVal(datasource.allCategories)
+    }
+}
