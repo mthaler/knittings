00e75c4ebe509095b159f99f8bb7f6af627b7362
Michael Thaler <michael.thaler@dlr.de>
2020-04-11 07:54:08 +0200

    Refactored DeleteDialog.

--- app/src/main/kotlin/com/mthaler/knittings/category/DeleteDialog.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/DeleteDialog.kt
@@ -6,11 +6,10 @@ import com.mthaler.knittings.R
 
 object DeleteDialog {
 
-    fun create(context: Context, categoryName: String, onDelete: () -> Unit): AlertDialog {
+    fun create(context: Context, name: String, onDelete: () -> Unit): AlertDialog {
         val b = AlertDialog.Builder(context)
-        b.setTitle(R.string.delete_category_dialog_title)
-        b.setMessage(context.resources.getString(R.string.delete_category_dialog_question, categoryName))
-        b.setPositiveButton(context.resources.getString(R.string.delete_category_dialog_delete_button), { diaglog, which -> onDelete() })
+        b.setMessage(context.resources.getString(R.string.delete_dialog_question, name))
+        b.setPositiveButton(context.resources.getString(R.string.delete_dialog_delete_button), { diaglog, which -> onDelete() })
         b.setNegativeButton(R.string.dialog_button_cancel, { dialog, which -> })
         return b.create()
     }
