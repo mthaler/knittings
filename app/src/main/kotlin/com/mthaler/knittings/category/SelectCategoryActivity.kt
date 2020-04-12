382d6447e03627924846fbeb243663bc6ca4648e
Michael Thaler <michael.thaler@dlr.de>
2020-04-12 17:16:43 +0200

    Removed superfluous comments from SelectCategoryActivity.

--- app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/SelectCategoryActivity.kt
@@ -17,14 +17,6 @@ class SelectCategoryActivity : BaseActivity(), CategoryListFragment.OnFragmentIn
 
     var knittingID = -1L
 
-    /**
-     * Called when the activity is starting. This is where most initialization should go: calling setContentView(int)
-     * to inflate the activity's UI, using findViewById(int) to programmatically interact with widgets in the UI,
-     * calling managedQuery(android.net.Uri, String[], String, String[], String) to retrieve cursors for data being displayed, etc.
-     *
-     * @param savedInstanceState Bundle: If the activity is being re-initialized after previously being shut down then this Bundle contains
-     *                           the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_category)
@@ -46,23 +38,11 @@ class SelectCategoryActivity : BaseActivity(), CategoryListFragment.OnFragmentIn
         }
     }
 
-    /**
-     * Called to ask the fragment to save its current dynamic state, so it can later be reconstructed in a
-     * new instance of its process is restarted. If a new instance of the fragment later needs to be created,
-     * the data you place in the Bundle here will be available in the Bundle given to onCreate(Bundle),
-     * onCreateView(LayoutInflater, ViewGroup, Bundle), and onActivityCreated(Bundle).
-     *
-     * @param outState If the fragment is being re-created from a previous saved state, this is the state.
-     */
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putLong(EXTRA_KNITTING_ID, knittingID)
         super.onSaveInstanceState(outState)
     }
 
-    /**
-     * Called when the activity has detected the user's press of the back key. The default implementation simply
-     * finishes the current activity, but you can override this to do whatever you want.
-     */
     override fun onBackPressed() {
         val fm = supportFragmentManager
         val f = fm.findFragmentById(R.id.select_category_container)
@@ -74,12 +54,6 @@ class SelectCategoryActivity : BaseActivity(), CategoryListFragment.OnFragmentIn
         super.onBackPressed()
     }
 
-    /**
-     * This hook is called whenever an item in your options menu is selected.
-     *
-     * @param item the menu item that was selected.
-     * @return return false to allow normal menu processing to proceed, true to consume it here.
-     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         android.R.id.home -> {
             val fm = supportFragmentManager
