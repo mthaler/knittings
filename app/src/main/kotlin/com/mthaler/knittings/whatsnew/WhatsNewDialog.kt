7c01823ace33bb210378845eeacac1dc636f9d80
Michael Thaler <michael.thaler@dlr.de>
2020-04-15 12:11:51 +0200

    Updated WhatsNewDialog.

--- app/src/main/kotlin/com/mthaler/knittings/whatsnew/WhatsNewDialog.kt
+++ app/src/main/kotlin/com/mthaler/knittings/whatsnew/WhatsNewDialog.kt
@@ -3,6 +3,8 @@ package com.mthaler.knittings.whatsnew
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import com.mthaler.knittings.R
 
 object WhatsNewDialog {
@@ -11,6 +13,12 @@ object WhatsNewDialog {
         @SuppressLint("InflateParams")
         val layoutInflater = activity.layoutInflater
         val v = layoutInflater.inflate(R.layout.dialog_whats_new, null)
+        val versionHistory = v.findViewById<TextView>(R.id.version_history)
+        val text = HtmlCompat.fromHtml(
+                "TextView first line... <br />" +
+                        "<b>Bold Text</b> | <i>Italic Text</i> and <br/>" +
+                        "<u>Underlined text</u>", 0)
+        versionHistory.text = text
         val b = AlertDialog.Builder(activity)
         b.setView(v)
         b.setPositiveButton(android.R.string.ok) { diag, i -> diag.dismiss() }
