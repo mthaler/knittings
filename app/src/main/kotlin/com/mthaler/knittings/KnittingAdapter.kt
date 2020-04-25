5e95bee61202671bc5a6390ee35c98e604a35009
Michael Thaler <michael.thaler@dlr.de>
2020-04-25 07:41:45 +0200

    Added PhotoGalleryAdapter.

--- app/src/main/kotlin/com/mthaler/knittings/KnittingAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/KnittingAdapter.kt
@@ -19,39 +19,17 @@ class KnittingAdapter(
     private val onItemLongClick: (Knitting) -> Unit
 ) : RecyclerView.Adapter<KnittingAdapter.ViewHolder>() {
 
-    /**
-     * Creates, configures and returns a ViewHolder object for a particular row in the list
-     *
-     * @param parent a ViewGroup that will hold the views managed by the holder, mostly used for layout inflation
-     * @param viewType an int that is the particular view type we are using, for cases where we have multiple view types
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item_knitting, parent, false)
         return ViewHolder(context, v)
     }
 
-    /**
-     * Updates the ViewHolder based upon the model data for a certain position
-     *
-     * @param holder ViewHolder object that should be updated
-     * @param position model position
-     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(knittings[position])
     }
 
-    /**
-     * Returns the number of items in the recycler view
-     */
     override fun getItemCount(): Int = knittings.size
 
-    /**
-     * The ViewHolder class is responsible for binding data as needed from our model into the widgets
-     * for a row in our list
-     *
-     * @param context context
-     * @param itemView item view
-     */
     inner class ViewHolder(val context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val titleTextView = itemView.findViewById<TextView>(R.id.knitting_list_item_titleTextView)
         private val descriptionTextView = itemView.findViewById<TextView>(R.id.knitting_list_item_descriptionTextView)
---
+++ app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryAdapter.kt
@@ -0,0 +1,42 @@
+package com.mthaler.knittings.photo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mthaler.knittings.R
+import com.mthaler.knittings.model.Knitting
+import com.mthaler.knittings.model.Photo
+
+abstract class PhotoGalleryAdapter(val context: Context,
+                                   private val onItemClick: (Knitting) -> Unit,
+                                   private val onItemLongClick: (Knitting) -> Unit) : RecyclerView.Adapter<PhotoGalleryAdapter.ViewHolder>() {
+
+    private val photos = ArrayList<Photo>()
+
+    fun setPhotos(photos: List<Photo>) {
+        this.photos.clear()
+        this.photos.addAll(photos)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_layout, parent, false)
+        return ViewHolder(context, v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(context: Context, view: View) : RecyclerView.ViewHolder(view) {
+
+        var imageTitle: TextView = view.findViewById(R.id.text)
+        var image: ImageView = view.findViewById(R.id.image)
+    }
+}
