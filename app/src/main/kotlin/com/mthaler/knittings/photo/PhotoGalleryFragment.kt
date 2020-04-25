33da6d977635372ac3a76447d189b2aa3be9ac93
Michael Thaler <michael.thaler@dlr.de>
2020-04-25 09:42:57 +0200

    Photo gallery now uses 3 columns in landscape mode.

--- app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryFragment.kt
@@ -3,16 +3,17 @@ package com.mthaler.knittings.photo
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.*
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.mthaler.knittings.R
 import com.mthaler.knittings.Extras.EXTRA_KNITTING_ID
+import com.mthaler.knittings.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -61,9 +62,11 @@ class PhotoGalleryFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val photoGalleryAdapter = PhotoGalleryAdapter(requireContext(), viewLifecycleOwner.lifecycleScope, { photo -> listener?.photoClicked(photo.id) })
+        val orientation = this.resources.configuration.orientation
+        val columns = if (orientation == Configuration.ORIENTATION_PORTRAIT) 2 else 3
         view?.let {
             val gridView = it.findViewById<RecyclerView>(R.id.gridView)
-            val gridLayoutManager = GridLayoutManager(requireContext(), 2)
+            val gridLayoutManager = GridLayoutManager(requireContext(), columns)
             gridView.layoutManager = gridLayoutManager
             gridView.adapter = photoGalleryAdapter
         }
