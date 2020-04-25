a7624727a54d2c3ff78ce289577603bcbdd8b116
Michael Thaler <michael.thaler@dlr.de>
2020-04-25 10:31:05 +0200

    Fixed bug that caused crash when deleting photo.

--- app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/photo/PhotoGalleryAdapter.kt
@@ -18,7 +18,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 
-class PhotoGalleryAdapter(val context: Context, private val lifecycleScope: LifecycleCoroutineScope, private val onItemClick: (Photo) -> Unit) : RecyclerView.Adapter<PhotoGalleryAdapter.ViewHolder>() {
+class PhotoGalleryAdapter(context: Context, private val lifecycleScope: LifecycleCoroutineScope, private val onItemClick: (Photo) -> Unit) : RecyclerView.Adapter<PhotoGalleryAdapter.ViewHolder>() {
 
     private val photos = ArrayList<Photo>()
     private val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
@@ -34,14 +34,14 @@ class PhotoGalleryAdapter(val context: Context, private val lifecycleScope: Life
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_layout, parent, false)
-        return ViewHolder(context, v)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(photos[position])
     }
 
-    inner class ViewHolder(context: Context, view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         var title: TextView = view.findViewById(R.id.text)
         var image: ImageView = view.findViewById(R.id.image)
@@ -60,27 +60,37 @@ class PhotoGalleryAdapter(val context: Context, private val lifecycleScope: Life
                     val height = image.measuredHeight
                     val filename = photo.filename.absolutePath
                     lifecycleScope.launch {
-                        val (imageSize, rotatedPhoto) = withContext(Dispatchers.Default) {
-                            val imageSize = if (displayPhotoSize) File(filename).length() else 0L
-                            val orientation = PictureUtils.getOrientation(filename)
-                            val photo = PictureUtils.decodeSampledBitmapFromPath(filename, width, height)
-                            val rotatedPhoto = PictureUtils.rotateBitmap(photo, orientation)
-                            Pair(imageSize, rotatedPhoto)
-                        }
-                        image.setImageBitmap(rotatedPhoto)
-                        if (displayPhotoSize) {
-                            if (photo.description.isNotEmpty()) {
-                                title.visibility = View.VISIBLE
-                                title.text = photo.description + " (" + imageSize / 1024 + " KB)"
+                        val result = withContext(Dispatchers.Default) {
+                            val file = File(filename)
+                            if (file.exists()) {
+                                val imageSize = if (displayPhotoSize) File(filename).length() else 0L
+                                val orientation = PictureUtils.getOrientation(filename)
+                                val photo = PictureUtils.decodeSampledBitmapFromPath(filename, width, height)
+                                val rotatedPhoto = PictureUtils.rotateBitmap(photo, orientation)
+                                Pair(imageSize, rotatedPhoto)
                             } else {
-                                title.visibility = View.VISIBLE
-                                title.text = "" + imageSize / 1024 + " KB"
+                                null
+                            }
+                        }
+                        if (result != null) {
+                            val (imageSize, rotatedPhoto) = result
+                            image.setImageBitmap(rotatedPhoto)
+                            if (displayPhotoSize) {
+                                if (photo.description.isNotEmpty()) {
+                                    title.visibility = View.VISIBLE
+                                    title.text = photo.description + " (" + imageSize / 1024 + " KB)"
+                                } else {
+                                    title.visibility = View.VISIBLE
+                                    title.text = "" + imageSize / 1024 + " KB"
+                                }
+                            } else {
+                                if (photo.description.isNotEmpty()) {
+                                    title.visibility = View.VISIBLE
+                                    title.text = photo.description
+                                }
                             }
                         } else {
-                            if (photo.description.isNotEmpty()) {
-                                title.visibility = View.VISIBLE
-                                title.text = photo.description
-                            }
+                            image.setImageResource(R.drawable.outline_insert_photo_black_48dp)
                         }
                     }
                     return true
