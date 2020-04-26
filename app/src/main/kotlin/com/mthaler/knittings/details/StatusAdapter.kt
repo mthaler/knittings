a5a71b8ef160acdd4963c49290dbbe05281527ff
Michael Thaler <michael.thaler@dlr.de>
2020-04-26 08:51:56 +0200

    Added StatusAdapter.

---
+++ app/src/main/kotlin/com/mthaler/knittings/details/StatusAdapter.kt
@@ -0,0 +1,10 @@
+package com.mthaler.knittings.details
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.mthaler.knittings.R
+
+class StatusAdapter(context: Context, titles: Array<String>, images: Array<Int>) : ArrayAdapter<String>(context, R.layout.spinner_item_status) {
+
+    
+}
