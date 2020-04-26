45c577edaa3bcf5ba98af5420688be19f0011dda
Michael Thaler <michael.thaler@dlr.de>
2020-04-26 09:58:23 +0200

    Status spinner now shows status icons. Closes #27.

--- app/src/main/kotlin/com/mthaler/knittings/details/StatusAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/details/StatusAdapter.kt
@@ -5,26 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.mthaler.knittings.R
 import com.mthaler.knittings.model.Status
-import kotlinx.android.synthetic.main.spinner_item_status.view.*
 
 class StatusAdapter(context: Context) : ArrayAdapter<Status>(context, 0, Status.values()) {
 
     override fun getView(position: Int, recycledView: View?, parent: ViewGroup): View {
-        return this.createView(position, recycledView, parent)
+        val status = getItem(position)!!
+        val view = recycledView ?: LayoutInflater.from(context).inflate(R.layout.spinner_status_selected, parent, false)
+        val statusImage = view.findViewById<ImageView>(R.id.statusImage)
+        statusImage.setImageResource(Status.getDrawableResource(context, status))
+        val statusText = view.findViewById<TextView>(R.id.statusText)
+        statusText.text = Status.format(context, status)
+        return view
     }
 
     override fun getDropDownView(position: Int, recycledView: View?, parent: ViewGroup): View {
-        return this.createView(position, recycledView, parent)
-    }
-
-    private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
         val status = getItem(position)!!
-        val view = recycledView ?: LayoutInflater.from(context).inflate(R.layout.spinner_item_status, parent, false)
-        view.statusImage.setImageResource(Status.getDrawableResource(context, status))
-        view.statusText.text = Status.format(context, status)
+        val view = recycledView ?: LayoutInflater.from(context).inflate(R.layout.spinner_status_row, parent, false)
+        val statusImage = view.findViewById<ImageView>(R.id.statusImage)
+        statusImage.setImageResource(Status.getDrawableResource(context, status))
+        val statusText = view.findViewById<TextView>(R.id.statusText)
+        statusText.text = Status.format(context, status)
         return view
-
     }
 }
