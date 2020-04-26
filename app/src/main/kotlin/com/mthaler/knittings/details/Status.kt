4c8bd50a2792fcd1bfeaa6b24eb90d2ac9d58b34
Michael Thaler <michael.thaler@dlr.de>
2020-04-26 08:59:40 +0200

    More work on StatusAdapter.

---
+++ app/src/main/kotlin/com/mthaler/knittings/details/Status.kt
@@ -0,0 +1,3 @@
+package com.mthaler.knittings.details
+
+data class Status(val image: Int, val name: String)
--- app/src/main/kotlin/com/mthaler/knittings/details/StatusAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/details/StatusAdapter.kt
@@ -1,10 +1,29 @@
 package com.mthaler.knittings.details
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.spinner_item_status.view.*
 
-class StatusAdapter(context: Context, titles: Array<String>, images: Array<Int>) : ArrayAdapter<String>(context, R.layout.spinner_item_status) {
+class StatusAdapter(context: Context, statusList: List<Status>) : ArrayAdapter<Status>(context, 0, statusList) {
 
-    
+    override fun getView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        return this.createView(position, recycledView, parent)
+    }
+
+    override fun getDropDownView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        return this.createView(position, recycledView, parent)
+    }
+
+    private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        val status = getItem(position)
+        val view = recycledView ?: LayoutInflater.from(context).inflate(R.layout.spinner_item_status, parent, false)
+        view.statusImage.setImageResource(status.image)
+        view.statusText.text = status.name
+        return view
+
+    }
 }
