28842dbf93525868dc3268744b6f77ca4c4ac93b
Michael Thaler <michael.thaler@dlr.de>
2020-04-28 05:53:28 +0200

    Reworked settings screen. Closes #73.

--- app/src/main/kotlin/com/mthaler/knittings/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/settings/SettingsFragment.kt
@@ -1,100 +1,12 @@
 package com.mthaler.knittings.settings
 
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
-import android.view.View
-import android.view.ViewGroup
 import androidx.preference.*
 import com.mthaler.knittings.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    /**
-     * Called during onCreate(Bundle) to supply the preferences for this fragment. Subclasses are expected to call
-     * setPreferenceScreen(PreferenceScreen) either directly or via helper methods such as addPreferencesFromResource(int).
-     *
-     * @param savedInstanceState If the fragment is being re-created from a previous saved state, this is the state.
-     * @param rootKey If non-null, this preference fragment should be rooted at the PreferenceScreen with this key.
-     */
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.app_preferences)
-        findPreference<Preference>(resources.getString(R.string.key_share_photo_size))?.let {
-            bindPreferenceSummaryToValue(it)
-        }
-    }
-
-    // TODO: this is a hack to remove the left margin from the PreferenceCategory
-    override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
-        return object : PreferenceGroupAdapter(preferenceScreen) {
-            @SuppressLint("RestrictedApi")
-            override fun onBindViewHolder(holder: PreferenceViewHolder, position: Int) {
-                super.onBindViewHolder(holder, position)
-                val preference = getItem(position)
-                if (preference is PreferenceCategory)
-                    setZeroPaddingToLayoutChildren(holder.itemView)
-            }
-        }
-    }
-
-    private fun setZeroPaddingToLayoutChildren(view: View) {
-        if (view !is ViewGroup)
-            return
-        val childCount = view.childCount
-        for (i in 0 until childCount) {
-            setZeroPaddingToLayoutChildren(view.getChildAt(i))
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-                view.setPaddingRelative(0, view.paddingTop, view.paddingEnd, view.paddingBottom)
-            else
-                view.setPadding(0, view.paddingTop, view.paddingRight, view.paddingBottom)
-        }
-    }
-
-    companion object {
-
-        /**
-         * A preference value change listener that updates the preference's summary
-         * to reflect its new value.
-         */
-        private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
-            val stringValue = value.toString()
-
-            if (preference is ListPreference) {
-                // For list pref_sharing, look up the correct display value in
-                // the preference's 'entries' list.
-                val index = preference.findIndexOfValue(stringValue)
-
-                // Set the summary to reflect the new value.
-                preference.setSummary(if (index >= 0) preference.entries[index] else null)
-            } else {
-                // For all other pref_sharing, set the summary to the value's
-                // simple string representation.
-                preference.summary = stringValue
-            }
-            true
-        }
-
-        /**
-         * Binds a preference's summary to its value. More specifically, when the
-         * preference's value is changed, its summary (line of text below the
-         * preference title) is updated to reflect the value. The summary is also
-         * immediately updated upon calling this method. The exact display format is
-         * dependent on the type of preference.
-
-         * @see .sBindPreferenceSummaryToValueListener
-         */
-        private fun bindPreferenceSummaryToValue(preference: Preference) {
-            // Set the listener to watch for value changes.
-            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
-
-            // Trigger the listener immediately with the preference's
-            // current value.
-            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.context)
-                            .getString(preference.key, ""))
-        }
     }
 }
