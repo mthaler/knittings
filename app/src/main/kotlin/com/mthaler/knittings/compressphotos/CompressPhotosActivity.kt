761f2f5dd7637adf4020399e5b1bebc01f498e74
Michael Thaler <michael.thaler@dlr.de>
2020-04-28 14:15:04 +0200

    Updated CompressPhotosActivity.

--- app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivity.kt
@@ -1,13 +1,31 @@
 package com.mthaler.knittings.compressphotos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.mthaler.knittings.R
 
 class CompressPhotosActivity : AppCompatActivity() {
 
+    private val broadcastReceiver = CompressPhotosBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compress_photos)
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, IntentFilter("MY_ACTION"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver)
+    }
+
+    private fun sendBroadcast(view: View?) {
+        val intent = Intent("MY_ACTION")
+        intent.putExtra("data", "Hello World!")
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
 }
