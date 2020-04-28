13fd455c072f81980b49af1245f8c25201e279eb
Michael Thaler <michael.thaler@dlr.de>
2020-04-28 14:05:28 +0200

    Added CompressPhotosActivity.

---
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivity.kt
@@ -0,0 +1,13 @@
+package com.mthaler.knittings.compressphotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mthaler.knittings.R
+
+class CompressPhotosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compress_photos)
+    }
+}
