000287fc0c029edd6de0a3b444eccd429d133f9e
Michael Thaler <michael.thaler@dlr.de>
2020-04-29 13:13:00 +0200

    CompressPhotoActivity now supports up navigation.

--- app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivity.kt
@@ -3,9 +3,10 @@ package com.mthaler.knittings.compressphotos
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.NavUtils
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mthaler.knittings.BaseActivity
@@ -45,6 +46,20 @@ class CompressPhotosActivity : BaseActivity() {
             number_of_photos.setText(statistics.photos.toString())
             total_size.setText(Format.humanReadableByteCountBin(statistics.totalSize))
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            // Respond to the action bar's Up/Home button
+            val upIntent: Intent? = NavUtils.getParentActivityIntent(this)
+            if (upIntent == null) {
+                throw IllegalStateException("No Parent Activity Intent")
+            } else {
+                NavUtils.navigateUpTo(this, upIntent)
+            }
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     companion object {
