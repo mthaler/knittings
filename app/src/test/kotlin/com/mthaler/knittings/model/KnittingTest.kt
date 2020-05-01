6f8932ab501cb5f2ece2cf09c2f258004f10f113
Michael Thaler <michael.thaler@dlr.de>
2020-05-01 11:04:58 +0200

    Added KnittingTest.

---
+++ app/src/test/kotlin/com/mthaler/knittings/model/KnittingTest.kt
@@ -0,0 +1,32 @@
+package com.mthaler.knittings.model
+
+import android.os.Parcel
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mthaler.knittings.utils.SerializeUtils
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class KnittingTest {
+
+    @Test
+    fun testSerializeDeserialize() {
+        val c = GregorianCalendar()
+        c.set(2018, 0, 10)
+        val started = c.time
+        val k0 = Knitting(42, "knitting", "my first knitting", started, null, "3.0", "41.0", null, 5.0)
+        val k1 = SerializeUtils.deserialize<Knitting>(SerializeUtils.serialize(k0))
+        assertEquals(k0, k1)
+        val p0 = Photo(42, File("/tmp/photo1.jpg"), 43, "socks", null)
+        c.set(2018, 0, 11)
+        val finished = c.time
+        val k2 = Knitting(42, "knitting", "my first knitting", started, finished, "3.0", "41.0", p0, 5.0)
+        val k3 = SerializeUtils.deserialize<Knitting>(SerializeUtils.serialize(k2))
+        assertEquals(k2, k3)
+    }
+
+
+}
