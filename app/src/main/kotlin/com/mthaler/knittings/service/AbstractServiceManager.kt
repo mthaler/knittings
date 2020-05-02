a809ab7619727d1d3591bf6f06c05206a9fbdcf9
Michael Thaler <michael.thaler@dlr.de>
2020-05-02 10:37:31 +0200

    AbstractServiceManager no longer exposes MutableLiveData.

--- app/src/main/kotlin/com/mthaler/knittings/service/AbstractServiceManager.kt
+++ app/src/main/kotlin/com/mthaler/knittings/service/AbstractServiceManager.kt
@@ -1,19 +1,25 @@
 package com.mthaler.knittings.service
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mthaler.knittings.utils.setMutVal
 
 abstract class AbstractServiceManager {
 
-    val jobStatus: MutableLiveData<JobStatus> = MutableLiveData(JobStatus.Progress(0))
+    private val _jobStatus: MutableLiveData<JobStatus> = MutableLiveData(JobStatus.Progress(0))
+    private val _serviceStatus: MutableLiveData<ServiceStatus> = MutableLiveData(ServiceStatus.Stopped)
 
-    val serviceStatus: MutableLiveData<ServiceStatus> = MutableLiveData(ServiceStatus.Stopped)
+    val jobStatus: LiveData<JobStatus>
+        get() = _jobStatus
+
+    val serviceStatus: LiveData<ServiceStatus>
+        get() = _serviceStatus
 
     fun updateJobStatus(jobStatus: JobStatus) {
-        this.jobStatus.setMutVal(jobStatus)
+        this._jobStatus.setMutVal(jobStatus)
     }
 
     fun updateServiceStatus(serviceStatus: ServiceStatus) {
-        this.serviceStatus.setMutVal(serviceStatus)
+        this._serviceStatus.setMutVal(serviceStatus)
     }
 }
