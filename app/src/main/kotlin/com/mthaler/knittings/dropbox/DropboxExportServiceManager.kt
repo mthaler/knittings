a4a5098af58a70c7c5f9272632ae32996b90103f
Michael Thaler <michael.thaler@dlr.de>
2020-05-02 06:05:36 +0200

    Added DropboxExportServiceManager.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportServiceManager.kt
@@ -0,0 +1,25 @@
+package com.mthaler.knittings.dropbox
+
+import androidx.lifecycle.MutableLiveData
+import com.mthaler.knittings.utils.setMutVal
+
+class DropboxExportServiceManager {
+    val status: MutableLiveData<Status> = MutableLiveData(Status.Progress(0))
+
+    fun statusUpdated(status: Status) {
+        this.status.setMutVal(status)
+    }
+
+    companion object {
+
+        private var sServiceManager: DropboxExportServiceManager? = null
+
+        @Synchronized
+        fun getInstance(): DropboxExportServiceManager {
+            if (sServiceManager == null) {
+                sServiceManager = DropboxExportServiceManager()
+            }
+            return sServiceManager!!
+        }
+    }
+}
