5b5251cdce25ac55a004ab1d8c6a9c5c061b4317
Michael Thaler <michael.thaler@dlr.de>
2020-05-03 14:40:11 +0200

    Added DropboxExportViewModel.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportViewModel.kt
@@ -0,0 +1,11 @@
+package com.mthaler.knittings.dropbox
+
+import android.app.Application
+import com.mthaler.knittings.DatasourceViewModel
+
+class DropboxExportViewModel(application: Application) : DatasourceViewModel(application)  {
+
+    override fun databaseChanged() {
+
+    }
+}
