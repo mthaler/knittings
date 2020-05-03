d5e913cfd885ba833e0c111833db30d110f0c33e
Michael Thaler <michael.thaler@dlr.de>
2020-05-03 14:45:07 +0200

    Implemented DropboxExportViewModel.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportViewModel.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportViewModel.kt
@@ -1,11 +1,44 @@
 package com.mthaler.knittings.dropbox
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.mthaler.knittings.DatasourceViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class DropboxExportViewModel(application: Application) : DatasourceViewModel(application)  {
 
-    override fun databaseChanged() {
+    val statistics = MutableLiveData(Statistics.EMPTY)
 
+    init {
+        updatePhotoCount()
+    }
+
+    override fun databaseChanged() {
+        updatePhotoCount()
+    }
+
+    private fun updatePhotoCount() {
+        viewModelScope.launch {
+            val s = withContext(Dispatchers.IO) {
+                val photos = datasource.allPhotos
+                var totalSize = 0L
+                for (p in photos) {
+                    try {
+                        if (p.filename.exists()) {
+                            val size = p.filename.length()
+                            totalSize += size
+                        }
+                    } catch(ex: Exception) {
+                        // ignore
+                    }
+                }
+                Statistics(photos.size, totalSize)
+            }
+            statistics.value = s
+        }
     }
 }
