2c390c7ac541c6ed9715131cccc2caa737b727ef
Michael Thaler <michael.thaler@dlr.de>
2020-05-03 07:16:25 +0200

    Added canceled property to AbstractServiceManager.

--- app/src/main/kotlin/com/mthaler/knittings/service/AbstractServiceManager.kt
+++ app/src/main/kotlin/com/mthaler/knittings/service/AbstractServiceManager.kt
@@ -3,11 +3,13 @@ package com.mthaler.knittings.service
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mthaler.knittings.utils.setMutVal
+import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class AbstractServiceManager {
 
     private val _jobStatus: MutableLiveData<JobStatus> = MutableLiveData(JobStatus.Progress(0))
     private val _serviceStatus: MutableLiveData<ServiceStatus> = MutableLiveData(ServiceStatus.Stopped)
+    private val _canceled = AtomicBoolean(false)
 
     val jobStatus: LiveData<JobStatus>
         get() = _jobStatus
@@ -20,6 +22,13 @@ abstract class AbstractServiceManager {
     }
 
     fun updateServiceStatus(serviceStatus: ServiceStatus) {
+        if (serviceStatus == ServiceStatus.Started) {
+            _canceled.set(false)
+        }
         this._serviceStatus.setMutVal(serviceStatus)
     }
+
+    var canceled: Boolean
+        get() = _canceled.get()
+        set(value) = _canceled.set(value)
 }
