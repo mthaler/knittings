a520bcf87f1a7439496b14a87f3e3231314a021c
Michael Thaler <michael.thaler@dlr.de>
2020-05-10 10:07:42 +0200

    Readded parcelable support to model classes.

--- app/src/main/kotlin/com/mthaler/knittings/model/Needle.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/Needle.kt
@@ -1,5 +1,9 @@
 package com.mthaler.knittings.model
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.mthaler.knittings.utils.readBoolean
+import com.mthaler.knittings.utils.writeBoolean
 import java.io.Serializable
 
 data class Needle(
@@ -11,10 +15,40 @@ data class Needle(
     val material: NeedleMaterial = NeedleMaterial.OTHER,
     val inUse: Boolean = false,
     val type: NeedleType = NeedleType.OTHER
-) : Serializable {
+) : Serializable, Parcelable {
+
+    private constructor(parcel: Parcel) : this(
+            id = parcel.readLong(),
+            name = parcel.readString(),
+            description = parcel.readString(),
+            size = parcel.readString(),
+            length = parcel.readString(),
+            material = NeedleMaterial.values()[parcel.readInt()],
+            inUse = parcel.readBoolean(),
+            type = NeedleType.values()[parcel.readInt()]
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(size)
+        parcel.writeString(length)
+        parcel.writeInt(material.ordinal)
+        parcel.writeBoolean(inUse)
+        parcel.writeInt(type.ordinal)
+    }
+
+    override fun describeContents(): Int = 0
 
     companion object {
 
         val EMPTY = Needle()
+
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Needle> {
+            override fun createFromParcel(parcel: Parcel) = Needle(parcel)
+            override fun newArray(size: Int) = arrayOfNulls<Needle>(size)
+        }
     }
 }
--- app/src/main/kotlin/com/mthaler/knittings/model/Photo.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/Photo.kt
@@ -1,6 +1,8 @@
 package com.mthaler.knittings.model
 
 import android.graphics.Bitmap
+import android.os.Parcel
+import android.os.Parcelable
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.Serializable
@@ -9,9 +11,34 @@ import java.io.Serializable
  * The photo class represents a photo. It has an id (used in the database), a filename and an
  * optional preview that can be displayed in the knittings list
  */
-data class Photo(val id: Long, val filename: File, val knittingID: Long, val description: String = "", val preview: Bitmap? = null) : Serializable {
+data class Photo(val id: Long, val filename: File, val knittingID: Long, val description: String = "", val preview: Bitmap? = null) : Serializable, Parcelable {
+
+    private constructor(parcel: Parcel) : this(
+            id = parcel.readLong(),
+            filename = File(parcel.readString()),
+            knittingID = parcel.readLong(),
+            description = parcel.readString(),
+            preview = parcel.readParcelable(null)
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(filename.absolutePath)
+        parcel.writeLong(knittingID)
+        parcel.writeString(description)
+        parcel.writeParcelable(preview, 0)
+
+    }
+
+    override fun describeContents(): Int = 0
 
     companion object {
+
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Photo> {
+            override fun createFromParcel(parcel: Parcel) = Photo(parcel)
+            override fun newArray(size: Int) = arrayOfNulls<Photo>(size)
+        }
 
         fun getBytes(preview: Bitmap?): ByteArray? {
             if (preview != null) {
