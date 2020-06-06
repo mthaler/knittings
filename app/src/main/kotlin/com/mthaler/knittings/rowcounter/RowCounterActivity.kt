9d4c7b417e03a8a802ceb269c4e7f990e9e59c97
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-06-06 13:28:35 +0200

    Number of rows can now be increased and decreased.

--- app/src/main/kotlin/com/mthaler/knittings/rowcounter/RowCounterActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/rowcounter/RowCounterActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.mthaler.knittings.R
 import kotlinx.android.synthetic.main.activity_project_count.*
 
@@ -19,14 +22,21 @@ class RowCounterActivity : AppCompatActivity() {
         // enable up navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val textViewRows = findViewById<TextView>(R.id.rows)
+
+        val viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(RowCounterViewModel::class.java)
+        viewModel.rows.observe(this, Observer { rows ->
+            textViewRows.setText(rows.toString())
+        })
+
         val plusButton = findViewById<Button>(R.id.button_plus)
         plusButton.setOnClickListener {
-
+            viewModel.rows.value = viewModel.rows.value!! + 1
         }
 
         val minusButton = findViewById<Button>(R.id.button_minus)
         minusButton.setOnClickListener {
-
+            viewModel.rows.value = viewModel.rows.value!! - 1
         }
     }
 
