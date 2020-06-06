9a532cb1b6ac1d1be9665cb76239b8cc3109282c
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-06-06 12:34:58 +0200

    Added row counter menu icon.

--- app/src/main/kotlin/com/mthaler/knittings/rowcounter/RowCounterActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/rowcounter/RowCounterActivity.kt
@@ -5,11 +5,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.activity_project_count.*
 
 class RowCounterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_row_counter)
+
+        setSupportActionBar(toolbar)
+
+        // enable up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     companion object {
