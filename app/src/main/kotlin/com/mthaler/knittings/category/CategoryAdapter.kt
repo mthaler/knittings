deb0223f021ebda517ec4f6468ec07a644d8aaa5
Michael Thaler <michael.thaler@dlr.de>
2020-09-06 14:32:49 +0200

    Moved ColorSwatch to DatabaseApplication.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.mthaler.dbapp.category.ColorSwatch
 import com.mthaler.knittings.R
 import com.mthaler.dbapp.model.Category
 
