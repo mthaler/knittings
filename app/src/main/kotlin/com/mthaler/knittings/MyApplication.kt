64ff89d59343175e4d105a4d620f78afa8953ca9
Michael Thaler <michael.thaler@dlr.de>
2020-09-06 18:44:12 +0200

    Initialize ObservableDatabase.

--- app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
@@ -2,6 +2,7 @@ package com.mthaler.knittings
 
 import androidx.multidex.MultiDexApplication
 import com.mthaler.dbapp.database.CategoryRepository
+import com.mthaler.dbapp.database.ObservableDatabase
 import com.mthaler.dbapp.database.PhotoRepository
 import com.mthaler.dbapp.settings.Theme
 import com.mthaler.knittings.database.KnittingsDataSource
@@ -12,6 +13,7 @@ class MyApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         KnittingsDataSource.init(this)
+        ObservableDatabase.init(KnittingsDataSource)
         CategoryRepository.init(KnittingsDataSource)
         PhotoRepository.init(KnittingsDataSource)
         Theme.setThemes(ThemeRepository.themes)
