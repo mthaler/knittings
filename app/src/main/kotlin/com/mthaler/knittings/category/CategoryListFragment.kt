64a51203cd6f031a1872a578961a60f568073bbf
Michael Thaler <michael.thaler@dlr.de>
2020-09-06 18:32:18 +0200

    Moved CategoryAdapter to DatabaseApplication.

--- app/src/main/kotlin/com/mthaler/knittings/category/CategoryListFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/category/CategoryListFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.mthaler.dbapp.category.CategoryAdapter
 import com.mthaler.dbapp.database.CategoryRepository
 import com.mthaler.knittings.DeleteDialog
 import com.mthaler.knittings.Extras
