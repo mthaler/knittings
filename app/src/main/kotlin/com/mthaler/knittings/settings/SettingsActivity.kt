6c43cc5353761db020e3e03a9da7cc41f793475d
Michael Thaler <michael.thaler@dlr.de>
2020-09-06 13:51:13 +0200

    Removed AbstractSettingsActivity.

--- app/src/main/kotlin/com/mthaler/knittings/settings/SettingsActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/settings/SettingsActivity.kt
@@ -2,11 +2,29 @@ package com.mthaler.knittings.settings
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import com.mthaler.dbapp.BaseActivity
 import com.mthaler.knittings.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity : AbstractSettingsActivity() {
+class SettingsActivity : BaseActivity() {
 
-    override fun viewID(): Int = R.id.settings_container
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        setSupportActionBar(toolbar)
+
+        // enable up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (savedInstanceState == null) {
+            val preferenceFragment = SettingsFragment()
+            val ft = supportFragmentManager.beginTransaction()
+            ft.add(R.id.settings_container, preferenceFragment)
+            ft.commit()
+        }
+    }
 
     companion object {
 
