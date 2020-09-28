5d9ebb2c64966f372611e0d98795fa64053aa9f7
Michael Thaler <michael.thaler@dlr.de>
2020-09-28 16:23:52 +0200

    Added MyApplication.getCategoryDataSource.

--- app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
@@ -2,7 +2,7 @@ package com.mthaler.knittings
 
 import androidx.multidex.MultiDexApplication
 import com.mthaler.dbapp.DatabaseApplication
-import com.mthaler.dbapp.database.CategoryRepository
+import com.mthaler.dbapp.database.CategoryDataSource
 import com.mthaler.dbapp.database.ObservableDatabase
 import com.mthaler.dbapp.database.PhotoDataSource
 import com.mthaler.dbapp.settings.Theme
@@ -15,11 +15,12 @@ class MyApplication : MultiDexApplication(), DatabaseApplication {
         super.onCreate()
         KnittingsDataSource.init(this)
         ObservableDatabase.init(KnittingsDataSource)
-        CategoryRepository.init(KnittingsDataSource)
         Theme.setThemes(ThemeRepository.themes)
     }
 
     override fun getFileProviderAuthority(): String = "com.mthaler.knittings.fileprovider"
+
+    override fun getCategoryDataSource(): CategoryDataSource = KnittingsDataSource
 
     override fun getPhotoDataSource(): PhotoDataSource = KnittingsDataSource
 }
