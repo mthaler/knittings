9e538cb4a07ec935cb8d821b48c6100f21592165
Michael Thaler <michael.thaler@dlr.de>
2020-09-28 08:23:05 +0200

    Added MyApplication.getPhotoDataSource.

--- app/src/main/kotlin/com/mthaler/knittings/Extras.kt
+++ app/src/main/kotlin/com/mthaler/knittings/Extras.kt
@@ -4,5 +4,4 @@ object Extras {
     const val EXTRA_CATEGORY_ID = "com.mthaler.knittings.CATEGORY_ID"
     const val EXTRA_KNITTING_ID = "com.mthaler.knittings.KNITTING_ID"
     const val EXTRA_NEEDLE_ID = "com.mthaler.knittings.NEEDLE_ID"
-    const val EXTRA_PHOTO_ID = "com.mthaler.knittings.PHOTO_ID"
 }
--- app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
+++ app/src/main/kotlin/com/mthaler/knittings/MyApplication.kt
@@ -4,7 +4,7 @@ import androidx.multidex.MultiDexApplication
 import com.mthaler.dbapp.DatabaseApplication
 import com.mthaler.dbapp.database.CategoryRepository
 import com.mthaler.dbapp.database.ObservableDatabase
-import com.mthaler.dbapp.database.PhotoRepository
+import com.mthaler.dbapp.database.PhotoDataSource
 import com.mthaler.dbapp.settings.Theme
 import com.mthaler.knittings.database.KnittingsDataSource
 import com.mthaler.knittings.settings.ThemeRepository
@@ -16,9 +16,10 @@ class MyApplication : MultiDexApplication(), DatabaseApplication {
         KnittingsDataSource.init(this)
         ObservableDatabase.init(KnittingsDataSource)
         CategoryRepository.init(KnittingsDataSource)
-        PhotoRepository.init(KnittingsDataSource)
         Theme.setThemes(ThemeRepository.themes)
     }
 
     override fun getFileProviderAuthority(): String = "com.mthaler.knittings.fileprovider"
+
+    override fun getPhotoDataSource(): PhotoDataSource = KnittingsDataSource
 }
