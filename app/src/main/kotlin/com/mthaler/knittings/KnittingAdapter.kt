262b66bae0f2d9f5ca3691504e25d171ce631d55
Michael Thaler <michael.thaler@dlr.de>
2020-09-28 18:25:11 +0200

    Use CategoryIndicator from dbapp.

--- app/src/main/kotlin/com/mthaler/knittings/KnittingAdapter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/KnittingAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.mthaler.dbapp.CategoryIndicator
 import com.mthaler.knittings.model.Knitting
 import com.mthaler.knittings.model.Status
 import java.text.DateFormat
