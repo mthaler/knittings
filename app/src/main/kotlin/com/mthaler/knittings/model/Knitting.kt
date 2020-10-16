e25afa9c6895df927f31978d6c95c26526bee0c4
Michael Thaler <michael.thaler@dlr.de>
2020-10-16 08:23:40 +0200

    Knitting now implements Project.

--- app/src/main/kotlin/com/mthaler/knittings/model/Knitting.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/Knitting.kt
@@ -4,6 +4,7 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.mthaler.dbapp.model.Category
 import com.mthaler.dbapp.model.Photo
+import com.mthaler.dbapp.model.Project
 import com.mthaler.dbapp.utils.readOptionalLong
 import com.mthaler.dbapp.utils.writeOptionalLong
 import java.io.Serializable
@@ -13,19 +14,19 @@ import java.util.Date
  * The Knitting class stores data for a single knitting
  */
 data class Knitting(
-        val id: Long = -1,
+        override val id: Long = -1,
         val title: String = "",
         val description: String = "",
         val started: Date = Date(),
         val finished: Date? = null,
         val needleDiameter: String = "",
         val size: String = "",
-        val defaultPhoto: Photo? = null,
+        override val defaultPhoto: Photo? = null,
         val rating: Double = 0.0,
         val duration: Long = 0L,
-        val category: Category? = null,
+        override val category: Category? = null,
         val status: Status = Status.PLANNED
-) : Serializable, Parcelable {
+) : Serializable, Parcelable, Project {
 
     private constructor(parcel: Parcel) : this(
             id = parcel.readLong(),
