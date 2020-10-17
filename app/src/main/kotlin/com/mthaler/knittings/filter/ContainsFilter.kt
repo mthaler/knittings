35660c5bc9c1f78f60d6c87013819fc6c10fdfde
Michael Thaler <michael.thaler@dlr.de>
2020-10-17 10:21:25 +0200

    Made ContainsFilter generic.

--- app/src/main/kotlin/com/mthaler/knittings/filter/ContainsFilter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/filter/ContainsFilter.kt
@@ -1,13 +1,14 @@
 package com.mthaler.knittings.filter
 
 import com.mthaler.dbapp.filter.Filter
+import com.mthaler.dbapp.model.Project
 import com.mthaler.dbapp.utils.StringUtils
 import com.mthaler.knittings.model.Knitting
 
 /**
  * A filter that filters the project list by checking if the title or the description contains the given text
  */
-data class ContainsFilter(val text: String) : Filter<Knitting> {
+data class ContainsFilter<T : Project>(val text: String) : Filter<T> {
 
-    override fun filter(knittings: List<Knitting>): List<Knitting> = knittings.filter { StringUtils.containsIgnoreCase(it.title, text) || StringUtils.containsIgnoreCase(it.description, text) }
+    override fun filter(knittings: List<T>): List<T> = knittings.filter { StringUtils.containsIgnoreCase(it.title, text) || StringUtils.containsIgnoreCase(it.description, text) }
 }
