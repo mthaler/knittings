3dd98683b480d2604a18b88462061a23eaacd806
Michael Thaler <michael.thaler@dlr.de>
2020-12-04 10:04:06 +0100

    Added RowConterConverter.

---
+++ app/src/main/kotlin/com/mthaler/knittings/database/RowConterConverter.kt
@@ -0,0 +1,16 @@
+package com.mthaler.knittings.database
+
+import com.mthaler.dbapp.database.*
+import com.mthaler.knittings.database.table.RowCounterTable
+import com.mthaler.knittings.model.RowCounter
+
+object RowConterConverter {
+
+    fun convert(dbRow: Map<String, Any?>): RowCounter {
+        val id = dbRow.getLong(RowCounterTable.Cols.ID)
+        val totalRows = dbRow.getInt(RowCounterTable.Cols.TOTAL_ROWS)
+        val rowsPerRepeat = dbRow.getInt(RowCounterTable.Cols.ROWS_PER_REPEAT)
+        val knittingID = dbRow.getLong(RowCounterTable.Cols.KNITTING_ID)
+        return RowCounter(id, totalRows, rowsPerRepeat, knittingID)
+    }
+}
