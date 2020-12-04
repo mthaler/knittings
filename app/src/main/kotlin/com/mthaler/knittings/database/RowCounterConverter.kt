dacabbb562e1803294edf82d4f4a4d892e101202
Michael Thaler <michael.thaler@dlr.de>
2020-12-04 10:05:01 +0100

    Renamed RowConterConverter to RowCounterConverter.

--- app/src/main/kotlin/com/mthaler/knittings/database/RowCounterConverter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/RowCounterConverter.kt
@@ -4,7 +4,7 @@ import com.mthaler.dbapp.database.*
 import com.mthaler.knittings.database.table.RowCounterTable
 import com.mthaler.knittings.model.RowCounter
 
-object RowConterConverter {
+object RowCounterConverter {
 
     fun convert(dbRow: Map<String, Any?>): RowCounter {
         val id = dbRow.getLong(RowCounterTable.Cols.ID)
