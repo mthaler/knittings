55891893259f38931937d7e1e98e6e2e9ddd5619
Michael Thaler <michael.thaler@dlr.de>
2020-12-07 12:09:16 +0100

    Updated CircularButton.

--- app/src/main/kotlin/com/mthaler/knittings/rowcounter/CircularButton.kt
+++ app/src/main/kotlin/com/mthaler/knittings/rowcounter/CircularButton.kt
@@ -1,15 +1,28 @@
 package com.mthaler.knittings.rowcounter
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import com.mthaler.knittings.R
+import kotlin.math.min
 
-class CircularButton@JvmOverloads constructor(
+class CircularButton @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        // Paint styles used for rendering are initialized here. This
+        // is a performance optimization, since onDraw() is called
+        // for every screen refresh.
+        style = Paint.Style.FILL
+    }
+
+    private var radius = 0.0f // Radius of the circle.
 
     init {
         isFocusable = true
@@ -19,8 +32,18 @@ class CircularButton@JvmOverloads constructor(
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircularButton)
     }
 
+    override fun setPressed(pressed: Boolean) {
+        super.setPressed(pressed)
+        if (pressed) {
+            paint.color = Color.RED
+        } else {
+            paint.color = Color.BLUE
+        }
+    }
+
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
-        // do something
+        // Calculate the radius from the smaller of the width and height.
+        radius = (min(width, height) / 2.0 * 0.8).toFloat()
     }
 }
