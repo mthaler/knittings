6a6d16f571a327b359e4efde19abfd74d83906f1
Michael Thaler <michael.thaler@dlr.de>
2020-12-08 07:27:58 +0100

    Added CountFrame.

---
+++ app/src/main/kotlin/com/mthaler/knittings/rowcounter/CountFrame.kt
@@ -0,0 +1,12 @@
+package com.mthaler.knittings.rowcounter
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CountFrame @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+}
