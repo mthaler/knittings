0c70a4b048add77eef6eb270f885f855590fcbfa
Michael Thaler <michael.thaler@dlr.de>
2022-02-17 17:14:22 +0100

    Moved AbstractObservableDatabase to kotlin directoy.

--- app/src/test/java/com/mthaler/knittings/utils/TryTest.kt
+++ app/src/test/java/com/mthaler/knittings/utils/TryTest.kt
@@ -8,16 +8,16 @@ class TryTest {
 
     @Test
     fun testSuccess() {
-        val result = com.mthaler.knittings.utils.Try { 42 }
+        val result = Try { 42 }
         assertTrue(result.isSucesss())
         assertEquals(42, result.get())
     }
 
     @Test
     fun testFailure() {
-        val result = com.mthaler.knittings.utils.Try { 42 / 0 }
+        val result = Try { 42 / 0 }
         assertTrue(result.isFailure())
-        val failure = result as com.mthaler.knittings.utils.Try.Failure
+        val failure = result as Try.Failure
         assertTrue(failure.exception is ArithmeticException)
     }
 }
