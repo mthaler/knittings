0070d7a2da153754b35f6f6cbaac69f6be245f3e
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 11:35:39 +0100

    Updated CompressPhotoFragment again.

--- app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoFragment.kt
@@ -1,10 +1,12 @@
 package com.mthaler.knittings.compressphotos
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.work.Constraints
 import androidx.work.NetworkType
+import com.mthaler.knittings.R
 import com.mthaler.knittings.databinding.FragmentCompressPhotoBinding
 
 class CompressPhotoFragment : Fragment() {
@@ -16,7 +18,23 @@ class CompressPhotoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        binding.buttonStart.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            with(builder) {
+                setTitle(resources.getString(R.string.compress_photos_dialog_title))
+                setMessage(resources.getString(R.string.compress_photos_dialog_message))
+                setPositiveButton(resources.getString(R.string.compress_photos_dialog_button_compress)) { dialog, which ->
+                    val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+                    CompressPhotosService.startService(requireContext(), "Foreground Service is running...")
+                }
+                setNegativeButton(resources.getString(R.string.dialog_button_cancel)) { dialog, which -> }
+                show()
+            }
+        }
+
+        binding.cancelButton.setOnClickListener {
+            CompressPhotosServiceManager.getInstance().cancelled = true
+        }
     }
 
     override fun onDestroyView() {
