d58f8a86ab5946f1e79d638cb4addcda9c7b1ed3
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 18:37:59 +0100

    Updated CompressPhotoTest.

--- app/src/androidTest/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoTest.kt
+++ app/src/androidTest/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoTest.kt
@@ -1,12 +1,17 @@
 package com.mthaler.knittings.compressphotos
 
 import android.util.Log
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import androidx.work.testing.SynchronousExecutor
 import androidx.work.testing.WorkManagerTestInitHelper
-import org.junit.Before
+import org.hamcrest.core.Is.`is`
+import org.junit.*
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -23,4 +28,24 @@ class CompressPhotoTest {
         // Initialize WorkManager for instrumentation tests.
         WorkManagerTestInitHelper.initializeTestWorkManager(context, config)
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun testEchoWorkerNoInput() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+       // Create request
+       val request = OneTimeWorkRequestBuilder<EchoWorker>()
+           .build()
+
+       val workManager = WorkManager.getInstance(context)
+       // Enqueue and wait for result. This also runs the Worker synchronously
+       // because we are using a SynchronousExecutor.
+       workManager.enqueue(request).result.get()
+       // Get WorkInfo
+       val workInfo = workManager.getWorkInfoById(request.id).get()
+
+       // Assert
+       assertThat(workInfo.state, `is`(WorkInfo.State.FAILED))
+    }
+
 }
