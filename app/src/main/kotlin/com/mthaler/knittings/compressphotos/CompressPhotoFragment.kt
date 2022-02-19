77a618b4c7f99dd3130d9fa9001c394f5d10d3d5
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 11:27:59 +0100

    Updated CompressPhotoFragment again.

--- app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoFragment.kt
@@ -1,12 +1,21 @@
 package com.mthaler.knittings.compressphotos
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import com.mthaler.knittings.databinding.FragmentCompressPhotoBinding
 
 class CompressPhotoFragment : Fragment() {
 
     private var _binding: FragmentCompressPhotoBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
