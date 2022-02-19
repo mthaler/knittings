032a934974fdbb3d5d12909389d293939d2d5768
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 15:52:11 +0100

    Updated CompressPhotoWorker.

--- app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoWorker.kt
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoWorker.kt
@@ -1,21 +1,27 @@
 package com.mthaler.knittings.compressphotos
 
-import android.app.PendingIntent
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import com.mthaler.knittings.database.KnittingsDataSource
 import com.mthaler.knittings.service.JobStatus
 import com.mthaler.knittings.utils.FileUtils
 import com.mthaler.knittings.utils.PictureUtils
+import java.lang.Exception
 
 class CompressPhotoWorker(val context: Context, parameters: WorkerParameters) : CoroutineWorker(context, parameters) {
 
     override suspend fun doWork(): Result {
-        TODO("Not yet implemented")
+        try {
+            compressPhotos()
+            return Result.success()
+        } catch (ex: Exception) {
+            return Result.failure()
+        }
     }
 
-    private suspend fun compressPhotos(pendingIntent: PendingIntent) {
+    private suspend fun compressPhotos() {
         val sm = CompressPhotosServiceManager.getInstance()
         val photos = KnittingsDataSource.allPhotos
         val photosToCompress = photos.filter {
@@ -42,7 +48,12 @@ class CompressPhotoWorker(val context: Context, parameters: WorkerParameters) : 
                     error("Could not delete $compressed")
                 }
             }
+            setProgressAsync(Data.Builder().putInt(Progress, progress).build())
             sm.updateJobStatus(JobStatus.Progress(progress))
         }
+    }
+
+    companion object {
+        const val Progress = "Progress"
     }
 }
