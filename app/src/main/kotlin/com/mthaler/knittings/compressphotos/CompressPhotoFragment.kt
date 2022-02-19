e3fb8e5669dbe93b812d802acca4f9ba66d713b0
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 11:18:30 +0100

    Added CompressPhotoFragment.

---
+++ app/src/main/kotlin/com/mthaler/knittings/compressphotos/CompressPhotoFragment.kt
@@ -0,0 +1,8 @@
+package com.mthaler.knittings.compressphotos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.work.Constraints
+
+class CompressPhotoFragment : Fragment() {
+}
