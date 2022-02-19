de8c5eafc7ec67111e0dddbe22020c305e99d21e
Michael Thaler <michael.thaler@dlr.de>
2022-02-19 18:27:18 +0100

    Added EchoWorker.

---
+++ app/src/androidTest/kotlin/com/mthaler/knittings/compressphotos/EchoWorker.kt
@@ -0,0 +1,14 @@
+package com.mthaler.knittings.compressphotos
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class EchoWorker(context: Context, parameters: WorkerParameters) : Worker(context, parameters) {
+   override fun doWork(): Result {
+       return when(inputData.size()) {
+           0 -> Result.failure()
+           else -> Result.success(inputData)
+       }
+   }
+}
