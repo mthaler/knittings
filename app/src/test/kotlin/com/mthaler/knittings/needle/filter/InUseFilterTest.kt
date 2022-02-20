2673f1228b8716edd7888e1c0eb85e05d3c0ca1c
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 15:44:46 +0100

    Added InUseFilterTest.

---
+++ app/src/test/kotlin/com/mthaler/knittings/needle/filter/InUseFilterTest.kt
@@ -0,0 +1,19 @@
+package com.mthaler.knittings.needle.filter
+
+import com.mthaler.knittings.model.Needle
+import com.mthaler.knittings.model.NeedleMaterial
+import com.mthaler.knittings.model.NeedleType
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class InUseFilterTest {
+
+    @Test
+    fun filter() {
+        val f = InUseFilter(true)
+        val n0 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, true, NeedleType.ROUND)
+        val n1 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, false, NeedleType.CIRCULAR)
+        val filtered = f.filter(listOf(n0, n1))
+        assertEquals(listOf(n0), filtered)
+    }
+}
