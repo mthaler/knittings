1042ff351808423a96399f2af80d3f7367a46f63
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 15:26:57 +0100

    Added SingleTypeFilterTest.

---
+++ app/src/test/kotlin/com/mthaler/knittings/needle/filter/SingleTypeFilterTest.kt
@@ -0,0 +1,19 @@
+package com.mthaler.knittings.needle.filter
+
+import com.mthaler.knittings.model.Needle
+import com.mthaler.knittings.model.NeedleMaterial
+import com.mthaler.knittings.model.NeedleType
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class SingleTypeFilterTest {
+
+    @Test
+    fun filter() {
+        val f0 = SingleTypeFilter(NeedleType.ROUND)
+        val n0 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, true, NeedleType.ROUND)
+        val n1 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, true, NeedleType.CIRCULAR)
+        val filtered = f0.filter(listOf(n0, n1))
+        assertEquals(listOf<Needle>(n0), filtered)
+    }
+}
