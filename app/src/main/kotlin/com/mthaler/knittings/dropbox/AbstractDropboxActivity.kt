4a55a55aec1864021cfa86c1515e915e751af8d5
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 17:55:39 +0100

    Updated AbstractDropboxActivity.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxActivity.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxActivity.kt
@@ -1,17 +1,8 @@
 package com.mthaler.knittings.dropbox
 
-import com.dropbox.core.android.AuthActivity
 import com.mthaler.knittings.BaseActivity
 
 abstract class AbstractDropboxActivity : BaseActivity() {
-
-    private fun clearDropboxClient() {
-        //DropboxClientFactory.clearClient()
-    }
-
-    private fun clearAuthActivity() {
-        AuthActivity.result = null
-    }
 
     protected abstract fun updateFragment()
 }
