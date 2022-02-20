0b7946c046d434cc249ecbd4311e5ec1344b9989
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 11:43:45 +0100

    Updated CategoryAdapterTest.

--- app/src/androidTest/kotlin/com/mthaler/knittings/category/CategoryAdapterTest.kt
+++ app/src/androidTest/kotlin/com/mthaler/knittings/category/CategoryAdapterTest.kt
@@ -2,7 +2,10 @@ package com.mthaler.knittings.category
 
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
+import com.mthaler.knittings.model.Category
+import junit.framework.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 
 class CategoryAdapterTest {
 
@@ -13,5 +16,14 @@ class CategoryAdapterTest {
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
         adapter = CategoryAdapter({ c -> {} }, { c -> {} })
+    }
+
+    @Test
+    fun itemCount() {
+        val c0 = Category(42, "test", null)
+        val categories = ArrayList<Category>()
+        categories.add(c0)
+        adapter.setCategories(categories)
+        assertEquals(1, adapter.itemCount)
     }
 }
