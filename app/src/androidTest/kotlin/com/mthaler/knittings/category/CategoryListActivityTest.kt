a9fb4aea546b0a3fd0c4948cf403b70b9eb29e13
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 16:33:23 +0100

    Added CategoryListActivityTest and NeedleListActivityTest.

---
+++ app/src/androidTest/kotlin/com/mthaler/knittings/category/CategoryListActivityTest.kt
@@ -0,0 +1,16 @@
+package com.mthaler.knittings.category
+
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CategoryListActivityTest {
+
+      @Test
+      fun testCategoryListActivityRecreate() {
+          val scenario = launchActivity<CategoryListActivity>()
+          scenario.recreate()
+      }
+}
