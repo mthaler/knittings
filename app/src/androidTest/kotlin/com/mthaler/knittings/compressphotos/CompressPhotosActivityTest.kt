94b9b28a03a9d493ee454ce0b11db5010594ca37
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 16:40:24 +0100

    Added CompressPhotosActivityTest.

---
+++ app/src/androidTest/kotlin/com/mthaler/knittings/compressphotos/CompressPhotosActivityTest.kt
@@ -0,0 +1,16 @@
+package com.mthaler.knittings.compressphotos
+
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CompressPhotosActivityTest {
+
+    @Test
+    fun testCompressPhotosActivityRecreate() {
+        val scenario = launchActivity<CompressPhotosActivity>()
+        scenario.recreate()
+    }
+}
