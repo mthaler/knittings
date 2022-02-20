53e5a8b44b96ae9251cb88cf43313fae5c9c8cd2
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 15:13:23 +0100

    Added .

--- app/src/androidTest/kotlin/com/mthaler/knittings/category/EditCategoryFragmentTest.kt
+++ app/src/androidTest/kotlin/com/mthaler/knittings/category/EditCategoryFragmentTest.kt
@@ -1,32 +1,28 @@
 package com.mthaler.knittings.category
 
 import android.content.Context
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.mthaler.knittings.model.Category
-import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class CategoryAdapterTest {
+class EditCategoryFragmentTest {
 
     private lateinit var context: Context
-    private lateinit var adapter: CategoryAdapter
 
     @Before
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
-        adapter = CategoryAdapter({ c -> {} }, { c -> {} })
     }
 
     @Test
-    fun itemCount() {
-        val c0 = Category(42, "test", null)
-        val categories = ArrayList<Category>()
-        categories.add(c0)
-        adapter.setCategories(categories)
-        assertEquals(1, adapter.itemCount)
+    fun testEditCategoryFragmentRecreate() {
+         val scenario = launchFragmentInContainer<EditCategoryFragment>()
+         scenario.recreate()
     }
+
 }
