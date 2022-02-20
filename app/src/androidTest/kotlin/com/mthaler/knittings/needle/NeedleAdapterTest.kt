3fbd1d9af5d6b2f22422d16a7f50ef9bee79cd87
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 11:37:15 +0100

    Updated NeedleAdapterTest again.

--- app/src/androidTest/kotlin/com/mthaler/knittings/needle/NeedleAdapterTest.kt
+++ app/src/androidTest/kotlin/com/mthaler/knittings/needle/NeedleAdapterTest.kt
@@ -6,10 +6,12 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.mthaler.knittings.model.Needle
 import com.mthaler.knittings.model.NeedleMaterial
 import com.mthaler.knittings.model.NeedleType
+import com.mthaler.knittings.needle.NeedleAdapter.Companion.TypeHeader
+import com.mthaler.knittings.needle.NeedleAdapter.Companion.TypeItem
 import junit.framework.Assert.assertEquals
 import org.junit.*
 import org.junit.runner.RunWith
-import java.util.*
+import kotlin.collections.ArrayList
 
 @RunWith(AndroidJUnit4::class)
 class NeedleAdapterTest {
@@ -26,7 +28,24 @@ class NeedleAdapterTest {
     @Test
     fun itemCount() {
         val n0 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, true, NeedleType.SET)
+        val needles = ArrayList<Needle>();
+        needles.add(n0)
+        val items = NeedleAdapter.groupItems(context, needles)
+        adapter.setItems(items)
+        assertEquals(2, adapter.itemCount)
+    }
+
+    @Test
+    fun getItemViewType() {
         adapter.setItems(emptyList())
         assertEquals(0, adapter.itemCount)
+         val n0 = Needle(42, "needle", "my first needle", "5 mm", "20 cm", NeedleMaterial.METAL, true, NeedleType.SET)
+        val needles = ArrayList<Needle>();
+        needles.add(n0)
+           val items = NeedleAdapter.groupItems(context, needles)
+        adapter.setItems(items)
+        assertEquals(2, adapter.itemCount)
+        assertEquals(TypeHeader, adapter.getItemViewType(0))
+        assertEquals(TypeItem, adapter.getItemViewType(1))
     }
 }
