10c154405138db167ebb8d007ce39946902d7363
Michael Thaler <michael.thaler@dlr.de>
2022-02-20 18:31:44 +0100

    Added DropboxImportActivityTest.

---
+++ app/src/androidTest/kotlin/com/mthaler/knittings/dropbox/DropboxImportActivityTest.kt
@@ -0,0 +1,16 @@
+package com.mthaler.knittings.dropbox
+
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DropboxImportActivityTest {
+
+    @Test
+    fun testCategoryListActivityRecreate() {
+          val scenario = launchActivity<DropboxImportActivity>()
+          scenario.recreate()
+    }
+}
