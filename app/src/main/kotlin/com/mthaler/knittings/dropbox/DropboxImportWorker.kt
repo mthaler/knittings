36a53e2ee61a4768ff28df4d3b8a714eb15fbb4f
Michael Thaler <michael.thaler@dlr.de>
2022-02-26 14:44:13 +0100

    Added DropboxImportWorker.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportWorker.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportWorker.kt
@@ -1,24 +1,40 @@
 package com.mthaler.knittings.dropbox
 
-import android.app.PendingIntent
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dropbox.core.DbxRequestConfig
 import com.dropbox.core.oauth.DbxCredential
 import com.dropbox.core.v2.DbxClientV2
+import com.mthaler.knittings.R
 import com.mthaler.knittings.model.ExportDatabase
-import com.mthaler.knittings.model.Project
+import com.mthaler.knittings.model.Knitting
 import com.mthaler.knittings.service.JobStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
-class DropboxImportWorker(val context: Context, parameters: WorkerParameters) : Worker(context, parameters) {
-
+class DropboxImportWorker(val directory: String, val database: ExportDatabase<Knitting>, val context: Context, parameters: WorkerParameters) : Worker(context, parameters) {
 
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                try {
+                    downloadPhotos(database, directory)
+                } catch (ex: Exception) {
+                    Log.e(DropboxExportWorker.TAG, "Could not download: " + ex)
+                }
+            }
+            DropboxImportServiceManager.getInstance().updateJobStatus(JobStatus.Success(context.resources.getString(R.string.dropbox_import_completed)))
+        }
+
+        return Result.success()
     }
 
-     private fun downloadPhotos(database: ExportDatabase<Project>, directory: String, pendingIntent: PendingIntent) {
+     private fun downloadPhotos(database: ExportDatabase<Knitting>, directory: String) {
         val clientIdentifier = "Knittings"
         val requestConfig = DbxRequestConfig(clientIdentifier)
         val credential = getLocalCredential()
@@ -32,13 +48,15 @@ class DropboxImportWorker(val context: Context, parameters: WorkerParameters) : 
     }
 
     //deserialize the credential from SharedPreferences if it exists
-    protected fun getLocalCredential(): DbxCredential? {
+    private fun getLocalCredential(): DbxCredential? {
         val sharedPreferences = context.getSharedPreferences(KNITTINGS, Context.MODE_PRIVATE)
         val serializedCredential = sharedPreferences.getString("credential", null) ?: return null
         return DbxCredential.Reader.readFully(serializedCredential)
     }
 
     companion object {
+        const val TAG = "DropboxImportWorker"
+
         private const val KNITTINGS = "com.mthaler.knittings"
     }
 }
