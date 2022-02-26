a8b9dcec67200d589a0088c42ccd5b1fca04f9b4
Michael Thaler <michael.thaler@dlr.de>
2022-02-26 16:52:12 +0100

    Updated DropboxImportCreator.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportCreator.kt
@@ -0,0 +1,19 @@
+package com.mthaler.knittings.dropbox
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.mthaler.knittings.model.ExportDatabase
+import com.mthaler.knittings.model.Knitting
+
+class DropboxImportCreator(val directory: String, val database: ExportDatabase<Knitting>) : WorkerFactory() {
+
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+        DropboxImportWorker(directory,database, appContext, workerParameters)
+    }
+}
