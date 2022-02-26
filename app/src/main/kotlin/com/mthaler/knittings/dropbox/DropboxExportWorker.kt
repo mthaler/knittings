2253d4f11eaf1fba2b04d39d82c30ac093c5f575
Michael Thaler <michael.thaler@dlr.de>
2022-02-26 09:54:10 +0100

    Added DropboxExportWorker.

---
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportWorker.kt
@@ -0,0 +1,75 @@
+package com.mthaler.knittings.dropbox
+
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.dropbox.core.DbxRequestConfig
+import com.dropbox.core.oauth.DbxCredential
+import com.dropbox.core.v2.DbxClientV2
+import com.dropbox.core.v2.files.WriteMode
+import com.mthaler.knittings.DatabaseApplication
+import com.mthaler.knittings.model.ExportDatabase
+import com.mthaler.knittings.model.Photo
+import com.mthaler.knittings.model.Project
+import com.mthaler.knittings.service.JobStatus
+import java.io.ByteArrayInputStream
+import java.io.FileInputStream
+import com.mthaler.knittings.utils.FileUtils.getExtension
+
+class DropboxExportWorker(val context: Context, parameters: WorkerParameters) : Worker(context, parameters) {
+
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+
+    private fun upload(dir: String, pendingIntent: PendingIntent): Boolean {
+        val clientIdentifier = "Knittings"
+        val requestConfig = DbxRequestConfig(clientIdentifier)
+        val credential = getLocalCredential()
+        credential?.let {
+            val dropboxClient = DbxClientV2(requestConfig, credential)
+            val sm = DropboxExportServiceManager.getInstance()
+            dropboxClient.files().createFolderV2("/$dir")
+            val database = (applicationContext as DatabaseApplication<Project>).createExportDatabase().checkDatabase()
+            uploadDatabase(dropboxClient, dir, database)
+            // upload photos to dropbox
+            val count = database.photos.size
+            for ((index, photo) in database.photos.withIndex()) {
+                if (sm.cancelled) {
+                    return true
+                }
+                uploadPhoto(dropboxClient, dir, photo)
+                val progress = (index / count.toFloat() * 100).toInt()
+                sm.updateJobStatus(JobStatus.Progress(progress))
+            }
+        }
+        return false
+    }
+
+    private fun uploadDatabase(dbxClient: DbxClientV2, dir: String, database: ExportDatabase<Project>) {
+        val dbJSON = database.toJSON()
+        val s = dbJSON.toString(2)
+        val dbInputStream = ByteArrayInputStream(s.toByteArray())
+        // upload database to dropbox
+        dbxClient.files().uploadBuilder("/$dir/db.json") // Path in the user's Dropbox to save the file.
+                .withMode(WriteMode.OVERWRITE) // always overwrite existing file
+                .uploadAndFinish(dbInputStream)
+    }
+
+    private fun uploadPhoto(dbxClient: DbxClientV2, dir: String, photo: Photo) {
+        val inputStream = FileInputStream(photo.filename)
+        dbxClient.files().uploadBuilder("/" + dir + "/" + photo.id + "." + getExtension(photo.filename.name)) // Path in the user's Dropbox to save the file.
+                .withMode(WriteMode.OVERWRITE) // always overwrite existing file
+                .uploadAndFinish(inputStream)
+    }
+
+
+    //deserialize the credential from SharedPreferences if it exists
+    protected fun getLocalCredential(): DbxCredential? {
+        val sharedPreferences = context.getSharedPreferences(DropboxExportService.KNITTINGS, Activity.MODE_PRIVATE)
+        val serializedCredential = sharedPreferences.getString("credential", null) ?: return null
+        return DbxCredential.Reader.readFully(serializedCredential)
+    }
+}
