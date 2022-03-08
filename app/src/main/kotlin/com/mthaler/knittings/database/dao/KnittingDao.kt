ac7137c6677bb3120cf72f51231c89051ff79e68
Michael Thaler <michael.thaler@dlr.de>
2022-03-08 13:40:59 +0100

    Added KnittingDao.

---
+++ app/src/main/kotlin/com/mthaler/knittings/database/dao/KnittingDao.kt
@@ -0,0 +1,17 @@
+package com.mthaler.knittings.database.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.mthaler.knittings.model.Knitting
+
+class KnittingDao {
+    @Insert
+    fun insertAll(vararg knittings: Knitting)
+
+    @Delete
+    fun delete(knitting: Knitting)
+
+    @Query("SELECT * FROM KNITTINGS")
+    fun getAll(): List<Knitting>
+}
