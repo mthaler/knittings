5f8b9f7c8884c6ee88b10c0462dde864e5836467
Michael Thaler <michael.thaler@dlr.de>
2022-03-08 13:45:26 +0100

    Added PhotoDao.

---
+++ app/src/main/kotlin/com/mthaler/knittings/database/dao/PhotoDao.kt
@@ -0,0 +1,19 @@
+package com.mthaler.knittings.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.mthaler.knittings.model.Photo
+
+@Dao
+interface PhotoDao {
+    @Insert
+    fun insertAll(vararg photos: Photo)
+
+    @Delete
+    fun delete(photo: Photo)
+
+    @Query("SELECT * FROM photos")
+    fun getAll(): List<Photo>
+}
