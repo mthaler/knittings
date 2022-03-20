3529da0c7614f750fa6b6b43688c5fda826d6b0a
Michael Thaler <michael.thaler@dlr.de>
2022-03-20 06:46:29 +0100

    Updated ProjectCountViewModelTest again.

--- app/src/test/kotlin/com/mthaler/knittings/filter/ContainsFilterTest.kt
+++ app/src/test/kotlin/com/mthaler/knittings/filter/ContainsFilterTest.kt
@@ -9,6 +9,8 @@ import java.util.*
 
 class ContainsFilterTest {
 
+
+
     @Test
      fun filter() {
         val c = GregorianCalendar()
--- app/src/test/kotlin/com/mthaler/knittings/projectcount/ProjectCountViewModelTest.kt
+++ app/src/test/kotlin/com/mthaler/knittings/projectcount/ProjectCountViewModelTest.kt
@@ -10,6 +10,21 @@ import java.util.*
 
 class ProjectCountViewModelTest {
 
+    @Test
+    fun testGetProjectCount() {
+        val c = GregorianCalendar()
+        c.set(2018, 0, 10)
+        val started = c.time
+        val k0 = Knitting(42, "knitting", "my first knitting", started, null, "3.0", "41.0", null, 5.0)
+        c.set(2018, 0, 11)
+        val finished = c.time
+        val p0 = Photo(42, File("/tmp/photo1.jpg"), 43, "socks", null)
+        val k1 = Knitting(42, "knitting", "my first knitting", started, finished, "3.0", "41.0", p0, 5.0)
+        val viewModel = ProjectCountViewModel()
+        val expected = viewModel.getProjectCount(listOf(k0, k1),2018, null)
+        assertEquals(expected, 1)
+    }
+
      @Test
      fun testCreateYearsList() {
         val c = GregorianCalendar()
