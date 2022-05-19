809a7444f564bbad6699fa2d7099bc224a653828
Michael Thaler <michael.thaler@dlr.de>
2022-05-19 07:55:23 +0200

    Updated StopwatchViewModelTest.

--- app/src/test/kotlin/com/mthaler/knittings/stopwatch/StopwatchViewModelTest.kt
+++ app/src/test/kotlin/com/mthaler/knittings/stopwatch/StopwatchViewModelTest.kt
@@ -1,9 +1,22 @@
 package com.mthaler.knittings.stopwatch
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.jraska.livedata.test
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class StopwatchViewModelTest {
+
+    @get:Rule
+    val schedulers = InstantTaskExecutorRule()
+
+    lateinit var viewModel: StopwatchViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = StopwatchViewModel()
+    }
 
     @Test
     fun time() {
