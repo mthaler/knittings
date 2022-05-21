3e506be0e5dc4699aad4c228281fa5fb7e50b391
Michael Thaler <michael.thaler@dlr.de>
2022-05-21 13:12:32 +0200

    Updated StopwatchFragment.

--- app/src/main/kotlin/com/mthaler/knittings/stopwatch/StopwatchFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/stopwatch/StopwatchFragment.kt
@@ -2,39 +2,47 @@ package com.mthaler.knittings.stopwatch
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.NavUtils
-import androidx.lifecycle.ViewModelProvider
 import com.mthaler.knittings.Extras.EXTRA_KNITTING_ID
 import com.mthaler.knittings.database.KnittingsDataSource
 import com.mthaler.knittings.databinding.FragmentStopwatchBinding
 import com.mthaler.knittings.model.Knitting
+import java.util.*
 
 class StopwatchFragment : Fragment() {
 
     private var _binding: FragmentStopwatchBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewModel: StopwatchViewModel
     private var knittingID: Long = Knitting.EMPTY.id
+
+    private var elapsedTime = 0L
+    private var previousTime = 0L
+    private var running = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProvider(requireActivity()).get(StopwatchViewModel::class.java)
-
         val a = arguments
         if (a != null) {
             knittingID = a.getLong(EXTRA_KNITTING_ID)
-            viewModel.elapsedTime = KnittingsDataSource.getProject(knittingID).duration
+        }
+
+
+        if (savedInstanceState != null) {
+            val t = savedInstanceState.getLong(EXTRA_STOPWATCH_ACTIVITY_STOPPED_TIME)
+            elapsedTime = savedInstanceState.getLong(EXTRA_STOPWATCH_ELAPSED_TIME) + (System.currentTimeMillis() - t)
+            running = savedInstanceState.getBoolean(EXTRA_STOPWATCH_RUNNING)
         }
 
         setHasOptionsMenu(true)
 
-        viewModel.runTimer()
+        runTimer()
     }
 
     override fun onCreateView(
@@ -45,24 +53,20 @@ class StopwatchFragment : Fragment() {
         _binding = FragmentStopwatchBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        viewModel.time.observe(viewLifecycleOwner, { time ->
-            binding.timeView.text = time
-        })
-
         binding.startButton.setOnClickListener {
-            viewModel.running = true
-            viewModel.previousTime = System.currentTimeMillis()
+            running = true
+            previousTime = System.currentTimeMillis()
         }
 
         binding.stopButton.setOnClickListener {
-            viewModel.running = false
+            running = false
             val knitting = KnittingsDataSource.getProject(knittingID)
-            KnittingsDataSource.updateProject(knitting.copy(duration = viewModel.elapsedTime))
+            KnittingsDataSource.updateProject(knitting.copy(duration = elapsedTime))
         }
 
         binding.discardButton.setOnClickListener {
-            viewModel.running = false
-            viewModel.elapsedTime = 0
+            running = false
+            elapsedTime = 0
             requireActivity().finish()
         }
 
@@ -84,7 +88,33 @@ class StopwatchFragment : Fragment() {
         else -> super.onOptionsItemSelected(item)
     }
 
+      private fun runTimer() {
+        previousTime = System.currentTimeMillis()
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val totalSeconds = elapsedTime / 1000
+                val hours = totalSeconds / 3600
+                val minutes = totalSeconds % 3600 / 60
+                val secs = totalSeconds % 60
+                val time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
+                binding.timeView.text = time
+                if (running) {
+                    val t = System.currentTimeMillis()
+                    elapsedTime += (t - previousTime)
+                    previousTime = t
+                }
+                handler.postDelayed(this, 500)
+            }
+        })
+    }
+
+
     companion object {
+        const val EXTRA_STOPWATCH_RUNNING = "com.mthaler.knitting.STOPWATCH_RUNNING"
+        const val EXTRA_STOPWATCH_ELAPSED_TIME = "com.mthaler.knitting.STOPWATCH_ELAPSED_TIME"
+        const val EXTRA_STOPWATCH_ACTIVITY_STOPPED_TIME = "com.mthaler.knitting.STOPWATCH_ACTIVITY_STOPPED_TIME"
+
         @JvmStatic
         fun newInstance(knittinhID: Long) =
             StopwatchFragment().apply {
