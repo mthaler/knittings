9f16bbdaf91ddf59643e5ae267c1e6ecaeda5d38
Michael Thaler <michael.thaler@dlr.de>
2022-05-22 16:48:46 +0200

    Updated AppDatabase.

--- app/src/main/kotlin/com/mthaler/knittings/database/AppDatabase.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/AppDatabase.kt
@@ -2,7 +2,11 @@ package com.mthaler.knittings.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.mthaler.knittings.database.dao.CategoryDao
 import com.mthaler.knittings.model.*
 
 @Database(entities = [Category::class, Knitting::class, Needle::class, Photo::class, RowCounter::class], version = 6)
-abstract class AppDatabase : RoomDatabase()
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun categoryDao(): CategoryDao
+}
