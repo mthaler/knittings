edd716be8156eb9c0a96c8b88c81bd0bd830cfb0
Michael Thaler <michael.thaler@dlr.de>
2022-05-22 15:36:45 +0200

    Updated AppDatabase.

--- app/src/main/kotlin/com/mthaler/knittings/database/AppDatabase.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/AppDatabase.kt
@@ -1,8 +1,8 @@
 package com.mthaler.knittings.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.mthaler.knittings.model.*
 
 @Database(entities = [Category::class, Knitting::class, Needle::class, Photo::class, RowCounter::class], version = 6)
-class AppDatabase {
-}
+abstract class AppDatabase : RoomDatabase()
