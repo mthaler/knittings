3a1bd3102ae186ca5e8de811a1e9a9f8205050ce
Michael Thaler <michael.thaler@dlr.de>
2022-06-07 12:50:07 +0200

    Updated BaseFragment again.

--- app/src/main/kotlin/com/mthaler/knittings/BaseFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/BaseFragment.kt
@@ -1,6 +1,7 @@
 package com.mthaler.knittings
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.mthaler.knittings.databinding.FragmentBaseBinding
 
@@ -11,6 +12,12 @@ class BaseFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        configureToolbar()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
