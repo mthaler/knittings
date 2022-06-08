6abd5a93a3ae8d8aa620feb484d968d6521bee2b
Michael Thaler <michael.thaler@dlr.de>
2022-06-08 07:13:59 +0200

    Added KnittingsStartupBenchmark again.

--- app/src/androidTest/kotlin/com/mthaler/knittings/KnittingsStartupBenchmark.kt
+++ app/src/androidTest/kotlin/com/mthaler/knittings/KnittingsStartupBenchmark.kt
@@ -1,10 +1,15 @@
 package com.mthaler.knittings
 
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class KnittingsStartupBenchmark {
+
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
 }
