177dd922ca8fe58ca47416c4123ca032173e63d9
Michael Thaler <michael.thaler@dlr.de>
2022-06-08 07:01:09 +0200

    Added KnittingsStartupBenchmark.

---
+++ app/src/androidTest/kotlin/com/mthaler/knittings/KnittingsStartupBenchmark.kt
@@ -0,0 +1,10 @@
+package com.mthaler.knittings
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class KnittingsStartupBenchmark {
+}
