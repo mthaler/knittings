dd12c5c89d625bd90b855c83f20bb5fba5253992
Michael Thaler <michael.thaler@dlr.de>
2022-06-13 09:42:03 +0200

    Updated RowCounter

--- app/src/main/kotlin/com/mthaler/knittings/model/RowCounter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/model/RowCounter.kt
@@ -1,5 +1,6 @@
 package com.mthaler.knittings.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.mthaler.knittings.database.table.RowCounterTable
@@ -8,9 +9,9 @@ import java.io.Serializable
 @Entity(tableName = RowCounterTable.ROW_COUNTERS)
 data class RowCounter(
     @PrimaryKey val id: Long = -1,
-    val totalRows: Int = 0,
-    val rowsPerRepeat: Int = 0,
-    val knittingID: Long = -1) : Serializable {
+    @ColumnInfo(name = "total_rows") val totalRows: Int = 0,
+    @ColumnInfo(name = "rows_per_repeat") val rowsPerRepeat: Int = 0,
+    @ColumnInfo(name = "knitting_id") val knittingID: Long = -1) : Serializable {
 
     companion object {
 
