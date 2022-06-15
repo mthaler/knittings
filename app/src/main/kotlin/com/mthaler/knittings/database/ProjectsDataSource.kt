fe86502542811303266be8b181017af75be8f390
Michael Thaler <michael.thaler@dlr.de>
2022-06-15 08:47:10 +0200

    Updated ProjectsDataSource

--- app/src/main/kotlin/com/mthaler/knittings/database/ProjectsDataSource.kt
+++ app/src/main/kotlin/com/mthaler/knittings/database/ProjectsDataSource.kt
@@ -1,17 +1,18 @@
 package com.mthaler.knittings.database
 
+import com.mthaler.knittings.model.Knitting
 import com.mthaler.knittings.model.Project
 import java.util.ArrayList
 
 interface ProjectsDataSource<T : Project> {
 
-    val allProjects: ArrayList<T>
+    val allProjects: ArrayList<Knitting>
 
     fun addProject(project: T, manualID: Boolean = false): T
 
     fun updateProject(project: T): T
 
-    fun deleteProject(project: T)
+    fun deleteProject(project: Knitting)
 
     fun deleteAllProjects()
 
