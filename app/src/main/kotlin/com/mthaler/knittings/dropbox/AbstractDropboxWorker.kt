843a03f05ee27096e4c2329900354c98496a9fea
Michael Thaler <michael.thaler@dlr.de>
2022-06-24 06:23:13 +0200

    Updated AbstractDropboxWorker

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxWorker.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/AbstractDropboxWorker.kt
@@ -1,8 +1,16 @@
 package com.mthaler.knittings.dropbox
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.dropbox.core.oauth.DbxCredential
 
-abstract class AbstractDropboxWorker(context: Context, parameters: WorkerParameters) : CoroutineWorker(context, parameters) {
+abstract class AbstractDropboxWorker(val context: Context, parameters: WorkerParameters) : CoroutineWorker(context, parameters) {
+
+    //serialize the credential and store in SharedPreferences
+    protected fun storeCredentialLocally(dbxCredential: DbxCredential) {
+        val sharedPreferences = context.getSharedPreferences("dropbox-sample", MODE_PRIVATE)
+        sharedPreferences.edit().putString("credential", dbxCredential.toString()).apply()
+    }
 }
