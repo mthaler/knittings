211f0aa397bb302ebd9f647e619fcde8ab30bb4e
Michael Thaler <michael.thaler@dlr.de>
2022-07-04 07:48:55 +0200

    Updated DropboxImportWorker

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportWorker.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxImportWorker.kt
@@ -89,8 +89,8 @@ class DropboxImportWorker(context: Context, parameters: WorkerParameters) : Abst
             // Download the file.
             val dropboxFilename = "/" + directory + "/" + photo.id + "." + FileUtils.getExtension("" + photo.filename)
             Log.d(TAG,"Saving file to $photo")
-            val localPath = storageDir.toPath().resolve(photo.filename.name)
-            FileOutputStream(localPath.toFile()).use {
+            val localPath = storageDir.resolve(photo.filename)
+            FileOutputStream(localPath).use {
                 dropboxClient.files().download(dropboxFilename).download(it)
                 Log.d(TAG, "Downloaded file  $photo")
             }
