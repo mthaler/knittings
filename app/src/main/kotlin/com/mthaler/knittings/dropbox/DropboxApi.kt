64c43ffee41b45d807a26e7e9b0aa14834120d90
Michael Thaler <michael.thaler@dlr.de>
2022-07-05 09:30:31 +0200

    Updated DropboxApi

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxApi.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxApi.kt
@@ -1,38 +1,17 @@
 package com.mthaler.knittings.dropbox
 
 import android.content.Context
-import android.os.Environment
-import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.LifecycleOwner
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkInfo
-import androidx.work.WorkManager
 import com.dropbox.core.DbxException
 import com.dropbox.core.v2.DbxClientV2
 import com.dropbox.core.v2.files.FileMetadata
 import com.dropbox.core.v2.files.ListFolderResult
 import com.dropbox.core.v2.files.Metadata
 import com.dropbox.core.v2.users.FullAccount
-import com.mthaler.knittings.DatabaseApplication
-import com.mthaler.knittings.R
-import com.mthaler.knittings.compressphotos.CompressPhotosServiceManager
-import com.mthaler.knittings.service.JobStatus
-import com.mthaler.knittings.service.ServiceStatus
-import com.mthaler.knittings.utils.FileUtils
-import com.mthaler.knittings.utils.WorkerUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import java.io.ByteArrayOutputStream
-import java.lang.Exception
 
 class DropboxApi(private val dropboxClient: DbxClientV2, val ctx: Context, val lifecycleOwner: LifecycleOwner) {
-
-    suspend fun listFolders(): ListFolderResult =
-        withContext(Dispatchers.IO) {
-            dropboxClient.files().listFolder("")
-        }
 
     suspend fun getAccountInfo(): DropboxAccountInfoResponse = withContext(Dispatchers.IO) {
         try {
