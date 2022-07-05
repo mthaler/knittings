8571a4d8524b873b4fe0f47a9b52da6dd27d9973
Michael Thaler <michael.thaler@dlr.de>
2022-07-05 12:36:43 +0200

    Updated DropboxExportWorker again.

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportWorker.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportWorker.kt
@@ -1,17 +1,12 @@
 package com.mthaler.knittings.dropbox
 
-import android.app.Activity
 import android.content.Context
 import android.util.Log
-import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.dropbox.core.DbxRequestConfig
-import com.dropbox.core.oauth.DbxCredential
 import com.dropbox.core.v2.DbxClientV2
 import com.dropbox.core.v2.files.WriteMode
-import com.mthaler.knittings.DatabaseApplication
-import com.mthaler.knittings.model.ExportDatabase
-import com.mthaler.knittings.model.Knitting
+import com.mthaler.knittings.database.KnittingsDataSource
+import com.mthaler.knittings.model.Database
 import com.mthaler.knittings.model.Photo
 import com.mthaler.knittings.service.JobStatus
 import java.io.ByteArrayInputStream
@@ -38,31 +33,45 @@ class DropboxExportWorker(context: Context, parameters: WorkerParameters) : Abst
         return Result.success()
     }
 
-    private fun upload(dir: String): Boolean {
-        val clientIdentifier = "Knittings"
-        val requestConfig = DbxRequestConfig(clientIdentifier)
-        val credential = getLocalCredential()
-        credential?.let {
-//            val dropboxClient = DbxClientV2(requestConfig, credential)
-//            val sm = DropboxExportServiceManager.getInstance()
-//            dropboxClient.files().createFolderV2("/$dir")
-//            val database = (applicationContext as DatabaseApplication).createExportDatabase().checkDatabase()
-//            uploadDatabase(dropboxClient, dir, database)
-//            // upload photos to dropbox
-//            val count = database.photos.size
-//            for ((index, photo) in database.photos.withIndex()) {
-//                if (sm.cancelled) {
-//                    return true
-//                }
-//                uploadPhoto(dropboxClient, dir, photo)
-//                val progress = (index / count.toFloat() * 100).toInt()
-//                sm.updateJobStatus(JobStatus.Progress(progress))
-//            }
+      private fun upload(dir: String): Boolean {
+        val dbxClient = DropboxClientFactory.getClient()
+        val sm = DropboxExportServiceManager.getInstance()
+        // create directory containing current date & time
+        dbxClient.files().createFolderV2("/$dir")
+        val database = checkDatabase(createDatabase())
+        uploadDatabase(dbxClient, dir, database)
+        // upload photos to dropbox
+        val count = database.photos.size
+        for ((index, photo) in database.photos.withIndex()) {
+            if (sm.cancelled) {
+                return true
+            }
+            uploadPhoto(dbxClient, dir, photo)
+            val progress = (index / count.toFloat() * 100).toInt()
+            sm.updateJobStatus(JobStatus.Progress(progress))
         }
         return false
     }
 
-    private fun uploadDatabase(dbxClient: DbxClientV2, dir: String, database: ExportDatabase) {
+    private fun createDatabase(): Database {
+        val knittings = KnittingsDataSource.allProjects
+        val photos = KnittingsDataSource.allPhotos
+        val categories = KnittingsDataSource.allCategories
+        val needles = KnittingsDataSource.allNeedles
+        val rowCounters = KnittingsDataSource.allRowCounters
+        return Database(knittings, photos, categories, needles, rowCounters)
+    }
+
+    private fun checkDatabase(database: Database): Database {
+        val filteredPhotos = database.photos.filter { it.filename.exists() }
+        val removedPhotos = database.photos.map { it.id }.toSet() - filteredPhotos.map { it.id}.toSet()
+        val updatedKnittings = database.knittings.map { if (removedPhotos.contains(it.defaultPhoto?.id)) it.copy(defaultPhoto = null) else it }
+        val filteredDatabase = database.copy(knittings = updatedKnittings, photos = filteredPhotos)
+        filteredDatabase.checkValidity()
+        return filteredDatabase
+    }
+
+    private fun uploadDatabase(dbxClient: DbxClientV2, dir: String, database: Database) {
         val dbJSON = database.toJSON()
         val s = dbJSON.toString(2)
         val dbInputStream = ByteArrayInputStream(s.toByteArray())
@@ -79,13 +88,12 @@ class DropboxExportWorker(context: Context, parameters: WorkerParameters) : Abst
                 .uploadAndFinish(inputStream)
     }
 
+
     private fun onUploadCompleted() {
         DropboxExportServiceManager.getInstance().updateJobStatus(JobStatus.Success())
     }
 
     companion object {
         val TAG = "DropboxExportWorker"
-
-        private const val KNITTINGS = "com.mthaler.knittings"
     }
 }
