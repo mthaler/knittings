e119f86f2c9307d5601b61fb239acc51e4894f7b
Michael Thaler <michael.thaler@dlr.de>
2022-07-06 12:36:28 +0200

    Updated TimeUtilsTest

--- app/src/test/kotlin/com/mthaler/knittings/utils/TimeUtilsTest.kt
+++ app/src/test/kotlin/com/mthaler/knittings/utils/TimeUtilsTest.kt
@@ -3,7 +3,6 @@ package com.mthaler.knittings.utils
 import org.junit.Test
 import org.junit.Assert.*
 import com.mthaler.knittings.utils.TimeUtils.formatDuration
-import com.mthaler.knittings.utils.TimeUtils.parseDuration
 
 class TimeUtilsTest {
 
@@ -16,16 +15,5 @@ class TimeUtilsTest {
         assertEquals("00:10:00", formatDuration(10 * 60 * 1000))
         assertEquals("01:00:00", formatDuration(1 * 60 * 60 * 1000))
         assertEquals("10:00:00", formatDuration(10 * 60 * 60 * 1000))
-    }
-
-    @Test
-    fun testParseDuration() {
-        assertEquals(0, parseDuration("00:00:00"))
-        assertEquals(1 * 1000, parseDuration("00:00:01"))
-        assertEquals(10 * 1000, parseDuration("00:00:10"))
-        assertEquals(1 * 60 * 1000, parseDuration("00:01:00"))
-        assertEquals(10 * 60 * 1000, parseDuration("00:10:00"))
-        assertEquals(1 * 60 * 60 * 1000, parseDuration("01:00:00"))
-        assertEquals(10 * 60 * 60 * 1000, parseDuration("10:00:00"))
     }
 }
