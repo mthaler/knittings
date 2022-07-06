c89cc099a0dbf41a7d740e447d6af550095320e7
Michael Thaler <michael.thaler@dlr.de>
2022-07-06 07:05:27 +0200

    Updated DropboxExportFragment

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportFragment.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportFragment.kt
@@ -135,8 +135,10 @@ class DropboxExportFragment : AbstractDropboxFragment() {
                     setTitle(resources.getString(R.string.dropbox_export))
                     setMessage(resources.getString(R.string.dropbox_export_no_wifi_question))
                     setPositiveButton(resources.getString(R.string.dropbox_export_dialog_export_button)) { _, _ ->
+                        val request = OneTimeWorkRequestBuilder<DropboxImportWorker>().build()
+                        val workManager = WorkManager.getInstance(requireContext())
+                        workManager.enqueueUniqueWork(TAG,  ExistingWorkPolicy.REPLACE, request)
                         DropboxExportServiceManager.getInstance().updateJobStatus(JobStatus.Progress(0))
-                        export()
                     }
                     setNegativeButton(resources.getString(R.string.dialog_button_cancel)) { _, _ -> }
                     show()
