d243dc6e6986c3a0d7ff07ac3c7315487a7530fe
Michael Thaler <michael.thaler@dlr.de>
2022-07-06 12:40:30 +0200

    Updated StringUtils.kt

--- app/src/main/kotlin/com/mthaler/knittings/filter/ContainsFilter.kt
+++ app/src/main/kotlin/com/mthaler/knittings/filter/ContainsFilter.kt
@@ -1,12 +1,12 @@
 package com.mthaler.knittings.filter
 
 import com.mthaler.knittings.model.Knitting
-import com.mthaler.knittings.utils.StringUtils
+import com.mthaler.knittings.utils.containsIgnoreCase
 
 /**
  * A filter that filters the project list by checking if the title or the description contains the given text
  */
 data class ContainsFilter(val text: String) : Filter<Knitting> {
 
-    override fun filter(items: List<Knitting>): List<Knitting> = items.filter { StringUtils.containsIgnoreCase(it.title, text) || StringUtils.containsIgnoreCase(it.description, text) }
+    override fun filter(items: List<Knitting>): List<Knitting> = items.filter { it.title.containsIgnoreCase(text) || it.description.containsIgnoreCase(text) }
 }
--- app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
+++ app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
@@ -1,25 +1,22 @@
 package com.mthaler.knittings.utils
 
-object StringUtils {
+fun String.containsIgnoreCase(what: String): Boolean {
+    val length = what.length
+    if (length == 0)
+        return true // Empty string is contained
 
-    fun containsIgnoreCase(src: String, what: String): Boolean {
-        val length = what.length
-        if (length == 0)
-            return true // Empty string is contained
+    val firstLo = Character.toLowerCase(what[0])
+    val firstUp = Character.toUpperCase(what[0])
 
-        val firstLo = Character.toLowerCase(what[0])
-        val firstUp = Character.toUpperCase(what[0])
+    for (i in this.length - length downTo 0) {
+        // Quick check before calling the more expensive regionMatches() method:
+        val ch = this[i]
+        if (ch != firstLo && ch != firstUp)
+            continue
 
-        for (i in src.length - length downTo 0) {
-            // Quick check before calling the more expensive regionMatches() method:
-            val ch = src[i]
-            if (ch != firstLo && ch != firstUp)
-                continue
-
-            if (src.regionMatches(i, what, 0, length, ignoreCase = true))
-                return true
-        }
-
-        return false
+        if (this.regionMatches(i, what, 0, length, ignoreCase = true))
+            return true
     }
+
+    return false
 }
