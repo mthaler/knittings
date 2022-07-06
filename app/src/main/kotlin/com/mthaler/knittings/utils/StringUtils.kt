baef1a5f677d801938135dac5930e3c759e23bc5
Michael Thaler <michael.thaler@dlr.de>
2022-07-06 12:15:27 +0200

    Updated StringUtils

--- app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
+++ app/src/main/kotlin/com/mthaler/knittings/utils/StringUtils.kt
@@ -2,17 +2,6 @@ package com.mthaler.knittings.utils
 
 object StringUtils {
 
-    /**
-     * Format bytes as a human-readable string
-     */
-    fun formatBytes(bytes: Long): String {
-        val unit = 1024
-        if (bytes < unit) return "$bytes B"
-        val exp = (Math.log(bytes.toDouble()) / Math.log(unit.toDouble())).toInt()
-        val pre = "KMGTPE"[exp - 1]
-        return String.format("%.1f %sB", bytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
-    }
-
     fun containsIgnoreCase(src: String, what: String): Boolean {
         val length = what.length
         if (length == 0)
