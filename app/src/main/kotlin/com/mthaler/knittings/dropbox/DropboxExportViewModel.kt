6ed6e156bbe09271fb0f64c90682cb8051046a94
Michael Thaler <michael.thaler@dlr.de>
2022-07-15 08:45:24 +0200

    Updated DropboxExportViewModel

--- app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportViewModel.kt
+++ app/src/main/kotlin/com/mthaler/knittings/dropbox/DropboxExportViewModel.kt
@@ -1,10 +1,10 @@
 package com.mthaler.knittings.dropbox
 
-import android.app.Application
 import android.os.Environment
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.mthaler.knittings.DatabaseApplication
+import com.mthaler.knittings.HasContext
 import com.mthaler.knittings.database.DataSourceViewModel
 import com.mthaler.knittings.database.KnittingsDataSource
 import com.mthaler.knittings.database.PhotoDataSource
@@ -16,6 +16,7 @@ import java.lang.Exception
 class DropboxExportViewModel : DataSourceViewModel()  {
 
     private val ds = KnittingsDataSource as PhotoDataSource
+    private val context = (KnittingsDataSource as HasContext).context
 
     val statistics = MutableLiveData(Statistics.EMPTY)
 
@@ -30,12 +31,13 @@ class DropboxExportViewModel : DataSourceViewModel()  {
     private fun updatePhotoCount() {
         viewModelScope.launch {
             val s = withContext(Dispatchers.IO) {
+                val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                Log.d(DropboxImportWorker.TAG, "storage dir: " + storageDir)
                 val photos = ds.allPhotos
                 var totalSize = 0L
                 for (p in photos) {
                     try {
                         if (p.filename.exists()) {
-
                             val size = p.filename.length()
                             totalSize += size
                         }
